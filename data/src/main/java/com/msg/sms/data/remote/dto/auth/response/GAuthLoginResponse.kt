8e4c54d42dde95fc7c7bda8ed16b8ecf28b103f1
khs3994 <z01039948003@gmail.com>
2023-04-20 10:20:44 +0900

    :memo: :: 필드명 변경

--- data/src/main/java/com/msg/sms/data/remote/dto/auth/response/GAuthLoginResponse.kt
+++ data/src/main/java/com/msg/sms/data/remote/dto/auth/response/GAuthLoginResponse.kt
@@ -3,6 +3,6 @@ package com.msg.sms.data.remote.dto.auth.response
 data class GAuthLoginResponse(
     val accessToken: String,
     val refreshToken: String,
-    val accessExp: String,
-    val refreshExp: String
+    val accessTokenExp: String,
+    val refreshTokenExp: String
 )
