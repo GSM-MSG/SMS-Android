9416178ff3c003407b92a39f395211de38efa74b
khs3994 <z01039948003@gmail.com>
2023-04-20 14:15:35 +0900

    :sparkles: :: API handler및 exception추가

---
+++ data/src/main/java/com/msg/sms/data/util/SMSApiHandler.kt
@@ -0,0 +1,57 @@
+package com.msg.sms.data.util
+
+import com.msg.sms.domain.exception.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+class SMSApiHandler<T> {
+    private lateinit var httpRequest: suspend () -> T
+
+    fun httpRequest(httpRequest: suspend () -> T) =
+        this.apply { this.httpRequest = httpRequest }
+
+    suspend fun sendRequest(): T {
+        return try {
+            withContext(Dispatchers.IO) {
+                httpRequest.invoke()
+            }
+        } catch (e: HttpException) {
+            val message = e.message
+            throw when (e.code()) {
+                400 -> BadRequestException(
+                    message = message
+                )
+                401 -> UnauthorizedException(
+                    message = message,
+                )
+                403 -> ForBiddenException(
+                    message = message,
+                )
+                404 -> NotFoundException(
+                    message = message,
+                )
+                409 -> ConflictException(
+                    message = message,
+                )
+                500, 501, 502, 503 -> ServerException(
+                    message = message,
+                )
+                else -> OtherHttpException(
+                    message = message,
+                    code = e.code()
+                )
+            }
+        } catch (e: SocketTimeoutException) {
+            throw TimeOutException(message = e.message)
+        } catch (e: UnknownHostException) {
+            throw NoInternetException()
+        } catch (e: NeedLoginException) {
+            throw NeedLoginException()
+        } catch (e: Exception) {
+            throw UnKnownException(message = e.message)
+        }
+    }
+}
