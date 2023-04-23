5f146e118f590b569e6a088378724faeb5d25e11
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-04-24 08:50:30 +0900

    ✨ :: add logic that auto login and addHeader to interceptor

---
+++ data/src/main/java/com/msg/sms/data/util/AuthInterceptor.kt
@@ -0,0 +1,52 @@
+package com.msg.sms.data.util
+
+import android.annotation.SuppressLint
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.msg.sms.data.local.datasource.auth.LocalAuthDataSource
+import com.msg.sms.domain.exception.NeedLoginException
+import com.sms.data.BuildConfig
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.text.SimpleDateFormat
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val dataSource: LocalAuthDataSource
+) : Interceptor {
+    @SuppressLint("SimpleDateFormat")
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd`T`HH:mm:ss")
+        val currentTime = dateFormat.format(System.currentTimeMillis()).toString()
+        runBlocking {
+            val refreshTime = dataSource.getRefreshTime().first()
+            val accessTime = dataSource.getAccessTime().first()
+
+            if (refreshTime >= currentTime) throw NeedLoginException()
+            if (accessTime >= currentTime) {
+                val client = OkHttpClient()
+                val refreshRequest = Request.Builder()
+                    .url(BuildConfig.BASE_URL + "auth")
+                    .addHeader("Refresh-Token", "Bearer ${dataSource.getRefreshToken().first()}")
+                    .build()
+                val jsonParser = JsonParser()
+                val response = client.newCall(refreshRequest).execute()
+                if (response.isSuccessful) {
+                    val token = jsonParser.parse(response.body!!.string()) as JsonObject
+                    dataSource.setAccessToken(token["accessToken"].toString())
+                    dataSource.setRefreshToken(token["refreshToken"].toString())
+                    dataSource.setAccessTime(token["accessTokenExp"].toString())
+                    dataSource.setRefreshTime(token["refreshTokenExp"].toString())
+                } else throw NeedLoginException()
+            }
+            builder.addHeader("Authorization", "Bearer ${dataSource.getAccessToken().first()}")
+        }
+
+        return chain.proceed(builder.build())
+    }
+}
