fabc7eb8a845989a75f5cd0675a6c9b0224fdf5e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-04-23 15:26:06 +0900

    ＋ :: remove the import code that don't used

--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
@@ -1,6 +1,5 @@
 package com.msg.sms.data.remote.datasource.auth
 
-import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 
