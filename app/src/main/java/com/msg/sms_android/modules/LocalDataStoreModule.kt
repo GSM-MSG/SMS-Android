95ae7d841571f3ed753754ba650e1838663dff3d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-04-23 15:52:52 +0900

    ✨ :: Use hilt to provide context and create preferenceDataStoreFactory

---
+++ app/src/main/java/com/msg/sms_android/modules/LocalDataStoreModule.kt
@@ -0,0 +1,31 @@
+package com.msg.sms_android.modules
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalDataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            produceFile = { context.preferencesDataStoreFile("autoDataStore") }
+        )
+    }
+}
