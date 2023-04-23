82c85fc58ec69956fec5e039f5a0182191a11d6c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-04-23 17:36:25 +0900

    ✨ :: set datastore interface

---
+++ data/src/main/java/com/msg/sms/data/local/datasource/auth/LocalAuthDataSource.kt
@@ -0,0 +1,35 @@
+package com.msg.sms.data.local.datasource.auth
+
+import kotlinx.coroutines.flow.Flow
+
+interface LocalAuthDataSource {
+
+    //    AccessToken
+    suspend fun getAccessToken(): Flow<String>
+
+    suspend fun setAccessToken(accessToken: String)
+
+    suspend fun removeAccessToken()
+
+    //    AccessTime
+    suspend fun getAccessTime(): Flow<String>
+
+    suspend fun setAccessTime(accessTime: String)
+
+    suspend fun removeAccessTime()
+
+    //    RefreshToken
+    suspend fun getRefreshToken(): Flow<String>
+
+    suspend fun setRefreshToken(refreshToken: String)
+
+    suspend fun removeRefreshToken()
+
+    //    RefreshTime
+    suspend fun getRefreshTime(): Flow<String>
+
+    suspend fun setRefreshTime(refreshTime: String)
+
+    suspend fun removeRefreshTime()
+
+}
