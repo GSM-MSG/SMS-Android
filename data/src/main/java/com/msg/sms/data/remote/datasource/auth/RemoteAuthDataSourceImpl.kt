e2084f0dc997978731efd0c7bf4665d1907b2fe8
khs3994 <z01039948003@gmail.com>
2023-04-24 10:25:28 +0900

    :fire: :: flow삭제

--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
@@ -5,21 +5,13 @@ import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.data.remote.network.api.AuthAPI
 import com.msg.sms.data.util.SMSApiHandler
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteAuthDataSourceImpl @Inject constructor(
     private val service: AuthAPI
 ) : RemoteAuthDataSource {
-    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponse> =
-        SMSApiHandler<Flow<GAuthLoginResponse>>()
-            .httpRequest {
-                flow {
-                    emit(service.gAuthLogin(body = LoginMapper.toLoginDto(body)))
-                }.flowOn(Dispatchers.IO)
-            }
+    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponse =
+        SMSApiHandler<GAuthLoginResponse>()
+            .httpRequest { service.gAuthLogin(body = LoginMapper.toLoginDto(body)) }
             .sendRequest()
 }
--- data/src/main/java/com/msg/sms/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/com/msg/sms/data/repository/AuthRepositoryImpl.kt
@@ -1,18 +1,16 @@
 package com.msg.sms.data.repository
 
 import com.msg.sms.data.remote.datasource.auth.RemoteAuthDataSource
+import com.msg.sms.data.remote.dto.auth.response.toLoginModel
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 import com.msg.sms.domain.model.auth.response.GAuthLoginResponseModel
 import com.msg.sms.domain.repository.AuthRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteAuthDataSource
 ) : AuthRepository {
-    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponseModel> {
-        return flow { remoteDataSource.gAuthLogin(body = body) }
+    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponseModel {
+        return remoteDataSource.gAuthLogin(body = body).toLoginModel()
     }
 }
-
