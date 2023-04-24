a6fc63afd98d5030c528aaf606463068a9f01fb2
khs3994 <z01039948003@gmail.com>
2023-04-24 10:09:30 +0900

    :sparkles: :: flow적용

--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
@@ -1,9 +1,9 @@
 package com.msg.sms.data.remote.datasource.auth
 
-import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
+import kotlinx.coroutines.flow.Flow
 
 interface RemoteAuthDataSource {
-    suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponse
+    suspend fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponse>
 }
--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
@@ -1,18 +1,25 @@
 package com.msg.sms.data.remote.datasource.auth
 
 import com.msg.sms.data.mapper.LoginMapper
-import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.data.remote.network.api.AuthAPI
 import com.msg.sms.data.util.SMSApiHandler
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteAuthDataSourceImpl @Inject constructor(
     private val service: AuthAPI
 ) : RemoteAuthDataSource {
-    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponse =
-        SMSApiHandler<GAuthLoginResponse>()
-            .httpRequest { service.gAuthLogin(body = LoginMapper.toLoginDto(body)) }
+    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponse> =
+        SMSApiHandler<Flow<GAuthLoginResponse>>()
+            .httpRequest {
+                flow {
+                    emit(service.gAuthLogin(body = LoginMapper.toLoginDto(body)))
+                }.flowOn(Dispatchers.IO)
+            }
             .sendRequest()
 }
