9f5c2ff3e77e186d1f44ea8da785e6cc4207f4f2
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-02 20:01:35 +0900

    ✨ :: create rounded button

---
+++ design-system/src/main/java/com/msg/sms/design/component/button/RoundedButton.kt
@@ -0,0 +1,12 @@
+package com.msg.sms.design.component.button
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RoundedButton(
+
+) {
+
+}
