6261fa48bb15b1bbb045abcd719961aea292f9c4
khs3994 <z01039948003@gmail.com>
2023-05-04 14:15:46 +0900

    :memo: :: click시 이벤트 추가

--- design-system/src/main/java/com/msg/sms/design/component/SmsChip.kt
+++ design-system/src/main/java/com/msg/sms/design/component/SmsChip.kt
@@ -1,16 +1,23 @@
 package com.msg.sms.design.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.msg.sms.design.theme.SMSTheme
+import com.msg.sms.design.theme.color.LightColor
 import com.sms.design_system.R
 
 @Composable
@@ -20,23 +27,44 @@ fun SmsChip(
     text: String,
     onClick: () -> Unit
 ) {
+    var backgroundColor by remember {
+        mutableStateOf(Color.Transparent)
+    }
+
     SMSTheme { colors, typography ->
-        Row(
-            modifier = modifier.clickable { onClick() },
-            verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .background(backgroundColor)
         ) {
-            Icon(
-                painter = painterResource(id = icon),
-                contentDescription = "Chip Component Icon",
-                tint = colors.N30
-            )
-            Spacer(modifier = Modifier.size(5.dp))
-            Text(
-                text = text,
-                style = typography.body1,
-                fontWeight = FontWeight.Normal,
-                color = colors.N30
-            )
+            Row(
+                modifier = modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
+                    .clickable { onClick() }
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onPress = {
+                                backgroundColor = LightColor.N20
+                                this.awaitRelease()
+                                backgroundColor = Color.Transparent
+                            },
+                        )
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = "Chip Component Icon",
+                    tint = colors.N30
+                )
+                Spacer(modifier = Modifier.size(5.dp))
+                Text(
+                    text = text,
+                    style = typography.body1,
+                    fontWeight = FontWeight.Normal,
+                    color = colors.N30
+                )
+            }
         }
     }
 }
