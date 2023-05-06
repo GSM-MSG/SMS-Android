0b8331e4e2d951af0a9173d1128cadf8c1610ad9
khs3994 <z01039948003@gmail.com>
2023-05-06 15:51:00 +0900

    :lipstick: :: 디자인 시스템 아이콘 추가

---
+++ design-system/src/main/java/com/msg/sms/design/icon/SmsIcon.kt
@@ -0,0 +1,118 @@
+package com.msg.sms.design.icon
+
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.sms.design_system.R
+
+@Composable
+fun BackButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_back_btn),
+        contentDescription = "Back Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun CameraIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_camera),
+        contentDescription = "Camera Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun CheckButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_check_btn),
+        contentDescription = "Check Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun DeleteButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_delete_btn),
+        contentDescription = "Delete Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun FilledPlusButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_filled_plus_btn),
+        contentDescription = "Filled Plus Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun OpenButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_open_btn),
+        contentDescription = "Open Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun PlusButtonIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_plus_btn),
+        contentDescription = "Plus Button Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun PlusButtonGrayIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_plus_btn_gray),
+        contentDescription = "Plus Button Gray Icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+
+
+
