b3d78ba7e0853f7022e046644c52f6773dea8d87
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-06 11:22:58 +0900

    ✨ :: add internal keyword

--- design-system/src/main/java/com/msg/sms/design/theme/SMSTheme.kt
+++ design-system/src/main/java/com/msg/sms/design/theme/SMSTheme.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import com.msg.sms.design.theme.color.LightColor
 
 @Composable
-fun SMSTheme(
+internal fun SMSTheme(
     colors: ColorTheme = if (true) LightColor else LightColor,
     typography: SMSTypography = SMSTypography,
     content: @Composable (colors: ColorTheme, typography: SMSTypography) -> Unit
