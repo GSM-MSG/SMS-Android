f2dcb98e2ffef04a2e62a5c62c034bb9141649f7
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-10 01:10:26 +0900

    ✨ :: add trailingIcon

--- design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
+++ design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
@@ -4,27 +4,29 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.msg.sms.design.icon.DeleteButtonIcon
 import com.msg.sms.design.theme.SMSTheme
 
 @Composable
 fun SmsTextField(
-    modifier: Modifier,
-    isError: Boolean,
-    placeHolder: String,
+    modifier: Modifier = Modifier,
+    isError: Boolean = false,
+    placeHolder: String = "",
     readOnly: Boolean = true,
-    focusRequester: FocusRequester,
-    errorText: String,
+    focusRequester: FocusRequester = FocusRequester(),
+    errorText: String = "Error",
     onValueChange: (String) -> Unit = {}
 ) {
     var text by remember { mutableStateOf("") }
@@ -48,7 +50,12 @@ fun SmsTextField(
                     placeholderColor = colors.N30,
                     focusedBorderColor = colors.P2,
                     unfocusedBorderColor = colors.N10
-                )
+                ),
+                trailingIcon = {
+                    IconButton(onClick = { text = "" }) {
+                        DeleteButtonIcon()
+                    }
+                }
             )
             if (isError) {
                 Spacer(modifier = Modifier.height(8.dp))
@@ -56,4 +63,16 @@ fun SmsTextField(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun SmsTextFieldPre() {
+    SmsTextField(
+        modifier = Modifier
+            .height(48.dp)
+            .width(200.dp),
+        placeHolder = "Test",
+        isError = true
+    )
 }
