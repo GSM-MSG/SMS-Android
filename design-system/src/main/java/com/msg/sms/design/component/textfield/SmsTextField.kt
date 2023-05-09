4d6340d18c8b6ddbc520e55cbd9da874587439b8
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-10 00:54:29 +0900

    ✨ :: set color each status

--- design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
+++ design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
@@ -1,9 +1,20 @@
 package com.msg.sms.design.component.textfield
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.unit.dp
 import com.msg.sms.design.theme.SMSTheme
 
 @Composable
@@ -12,22 +23,37 @@ fun SmsTextField(
     isError: Boolean,
     placeHolder: String,
     readOnly: Boolean = true,
-    isFocus: Boolean = false,
+    focusRequester: FocusRequester,
+    errorText: String,
     onValueChange: (String) -> Unit = {}
 ) {
     var text by remember { mutableStateOf("") }
     SMSTheme { colors, typography ->
-        TextField(
-            value = text,
-            onValueChange = {
-                text = it
-                onValueChange(it)
-            },
-            placeholder = {
-                Text(text = placeHolder, style = typography.body1)
-            },
-            modifier = modifier,
-            textStyle = typography.body1,
-        )
+        Column {
+            TextField(
+                value = text,
+                onValueChange = {
+                    text = it
+                    onValueChange(it)
+                },
+                placeholder = {
+                    Text(text = placeHolder, style = typography.body1)
+                },
+                modifier = modifier
+                    .border(width = 1.dp, shape = RoundedCornerShape(8.dp), color = colors.N10)
+                    .focusRequester(focusRequester),
+                textStyle = typography.body1,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    backgroundColor = colors.N10,
+                    placeholderColor = colors.N30,
+                    focusedBorderColor = colors.P2,
+                    unfocusedBorderColor = colors.N10
+                )
+            )
+            if (isError) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = errorText, color = colors.ERROR, style = typography.caption1)
+            }
+        }
     }
 }
