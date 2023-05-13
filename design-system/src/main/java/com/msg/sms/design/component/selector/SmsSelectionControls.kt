1aec7b7c46d822761f7eb415563fcfdaea3e05c8
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-14 01:34:50 +0900

    ✨ :: create component of SelectionControls

---
+++ design-system/src/main/java/com/msg/sms/design/component/selector/SmsSelectionControls.kt
@@ -0,0 +1,63 @@
+package com.msg.sms.design.component.selector
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.theme.SMSTheme
+
+@Composable
+fun SmsSelectionControls(
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: (() -> Unit)?
+) {
+    SMSTheme { colors, _ ->
+        Canvas(
+            modifier = modifier
+                .width(32.dp)
+                .height(32.dp)
+                .clickable {
+                    if (onClick != null && enabled) {
+                        onClick()
+                    }
+                },
+        ) {
+            drawCircle(
+                color = if (selected) colors.P2 else colors.N20,
+                radius = 10f,
+                style = Stroke(width = 2f)
+            )
+
+            drawCircle(
+                color = if (selected) colors.P2 else Color.Transparent,
+                radius = 6f
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SelectionControlPre() {
+    val selected = remember {
+        mutableStateOf("0")
+    }
+    Column(Modifier.background(Color.White)) {
+        SmsSelectionControls(selected = selected.value == "0", onClick = { selected.value = "0" })
+        Spacer(modifier = Modifier.height(10.dp))
+        SmsSelectionControls(selected = selected.value == "1", onClick = { selected.value = "1" })
+    }
+}
