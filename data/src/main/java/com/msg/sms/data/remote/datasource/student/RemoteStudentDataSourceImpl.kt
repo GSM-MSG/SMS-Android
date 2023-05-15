23810677f387eee4a0ab56582c967a08a52e0a01
khs3994 <z01039948003@gmail.com>
2023-05-15 20:53:42 +0900

    :sparkles: :: datasource 로직 작성

---
+++ data/src/main/java/com/msg/sms/data/remote/datasource/student/RemoteStudentDataSourceImpl.kt
@@ -0,0 +1,23 @@
+package com.msg.sms.data.remote.datasource.student
+
+import com.msg.sms.data.remote.dto.student.request.EnterStudentInformationRequest
+import com.msg.sms.data.remote.network.api.StudentAPI
+import com.msg.sms.data.util.SMSApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class RemoteStudentDataSourceImpl @Inject constructor(
+    private val service: StudentAPI
+) : RemoteStudentDataSource {
+    override suspend fun enterStudentInformation(body: EnterStudentInformationRequest): Flow<Unit> =
+        flow {
+            emit(
+                SMSApiHandler<Unit>()
+                    .httpRequest { service.enterStudentInformation(body = body) }
+                    .sendRequest()
+            )
+        }.flowOn(Dispatchers.IO)
+}
