0eed4095963a1e510d1de7c998c98fc67d5dfd9f
khs3994 <z01039948003@gmail.com>
2023-05-16 21:01:22 +0900

    :memo: :: auth 부분 repository패턴이 잘못 구현되어 있던 부분 수정

--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSource.kt
@@ -1,8 +1,9 @@
 package com.msg.sms.data.remote.datasource.auth
 
+import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 
 interface RemoteAuthDataSource {
-    suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponse
+    suspend fun gAuthLogin(body: GAuthLoginRequest): GAuthLoginResponse
 }
--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.msg.sms.data.remote.datasource.auth
 
 import com.msg.sms.data.mapper.LoginMapper
+import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.data.remote.network.api.AuthAPI
 import com.msg.sms.data.util.SMSApiHandler
@@ -10,8 +11,8 @@ import javax.inject.Inject
 class RemoteAuthDataSourceImpl @Inject constructor(
     private val service: AuthAPI
 ) : RemoteAuthDataSource {
-    override suspend fun gAuthLogin(body: GAuthLoginRequestModel): GAuthLoginResponse =
+    override suspend fun gAuthLogin(body: GAuthLoginRequest): GAuthLoginResponse =
         SMSApiHandler<GAuthLoginResponse>()
-            .httpRequest { service.gAuthLogin(body = LoginMapper.toLoginDto(body)) }
+            .httpRequest { service.gAuthLogin(body = body) }
             .sendRequest()
 }
