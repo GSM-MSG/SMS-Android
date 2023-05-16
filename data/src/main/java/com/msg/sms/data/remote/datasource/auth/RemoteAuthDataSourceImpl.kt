64285d4227d3105827a3d84504523a9330e9f57f
khs3994 <z01039948003@gmail.com>
2023-05-16 21:05:34 +0900

    :fire: :: mapper 삭제

--- data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/sms/data/remote/datasource/auth/RemoteAuthDataSourceImpl.kt
@@ -1,11 +1,9 @@
 package com.msg.sms.data.remote.datasource.auth
 
-import com.msg.sms.data.mapper.LoginMapper
 import com.msg.sms.data.remote.dto.auth.request.GAuthLoginRequest
 import com.msg.sms.data.remote.dto.auth.response.GAuthLoginResponse
 import com.msg.sms.data.remote.network.api.AuthAPI
 import com.msg.sms.data.util.SMSApiHandler
-import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 import javax.inject.Inject
 
 class RemoteAuthDataSourceImpl @Inject constructor(
