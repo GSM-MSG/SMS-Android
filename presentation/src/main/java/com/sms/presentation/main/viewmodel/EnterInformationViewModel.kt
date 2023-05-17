27599ccb3d61d41302363bfc6ade565ee37933a4
khs3994 <z01039948003@gmail.com>
2023-05-17 23:02:57 +0900

    :sparkles: :: 뷰모델 로직 작성

---
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/EnterInformationViewModel.kt
@@ -0,0 +1,64 @@
+package com.sms.presentation.main.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.msg.sms.domain.model.student.request.CertificateInformationModel
+import com.msg.sms.domain.model.student.request.EnterStudentInformationModel
+import com.msg.sms.domain.usecase.student.EnterStudentInformationUseCase
+import com.sms.presentation.main.viewmodel.util.Event
+import com.sms.presentation.main.viewmodel.util.errorHandling
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class EnterInformationViewModel @Inject constructor(
+    private val enterStudentInformationUseCase: EnterStudentInformationUseCase
+) : ViewModel() {
+    private val _enterInformationResponse = MutableLiveData<Event>()
+    val enterInformationResponse: LiveData<Event> get() = _enterInformationResponse
+
+    fun enterStudentInformation(
+        major: String,
+        techStack: List<String>,
+        profileImgUrl: String,
+        introduce: String,
+        stuNum: String,
+        portfolioUrl: String,
+        contactEmail: String,
+        formOfEmployment: String,
+        gsmAuthenticationScore: Int,
+        salary: Int,
+        region: List<String>,
+        languageCertificate: List<CertificateInformationModel>,
+        dreamBookFileUrl: String,
+        militaryService: String,
+        certificate: List<String>
+    ) = viewModelScope.launch {
+        enterStudentInformationUseCase(
+            EnterStudentInformationModel(
+                major = major,
+                techStack = techStack,
+                profileImgUrl = profileImgUrl,
+                introduce = introduce,
+                stuNum = stuNum,
+                portfolioUrl = portfolioUrl,
+                contactEmail = contactEmail,
+                formOfEmployment = formOfEmployment,
+                gsmAuthenticationScore = gsmAuthenticationScore,
+                salary = salary,
+                region = region,
+                languageCertificate = languageCertificate,
+                dreamBookFileUrl = dreamBookFileUrl,
+                militaryService = militaryService,
+                certificate = certificate
+            )
+        ).onSuccess {
+            it.collect {
+                _enterInformationResponse.value = Event.Success
+            }
+        }.onFailure {
+            _enterInformationResponse.value = it.errorHandling()
+        }
+    }
+}
