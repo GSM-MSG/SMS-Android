f4ef73d8f29c1367281013da5aa0cf781b100d07
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-22 14:04:11 +0900

    ✨ :: Add rounded corners to the bottom sheet

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/MilitaryServiceScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/MilitaryServiceScreen.kt
@@ -3,6 +3,7 @@ package com.sms.presentation.main.ui.fill_out_information
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.msg.sms.design.component.bottomsheet.SelectorBottomSheet
 import com.msg.sms.design.component.spacer.SmsSpacer
 import com.msg.sms.design.component.topbar.TopBarComponent
@@ -29,14 +31,18 @@ fun MilitaryServiceScreen() {
         mutableStateOf("")
     }
     val militaryServiceServiceList = listOf("병특 희망", "희망하지 않음", "상관없음", "해당 사항 없음")
-    ModalBottomSheetLayout(sheetContent = {
-        SelectorBottomSheet(
-            list = militaryServiceServiceList,
-            bottomSheetState = bottomSheetState,
-            selected = selectedMilitaryService.value,
-            itemChange = { selectedMilitaryService.value = it }
-        )
-    }, sheetState = bottomSheetState) {
+    ModalBottomSheetLayout(
+        sheetContent = {
+            SelectorBottomSheet(
+                list = militaryServiceServiceList,
+                bottomSheetState = bottomSheetState,
+                selected = selectedMilitaryService.value,
+                itemChange = { selectedMilitaryService.value = it }
+            )
+        },
+        sheetState = bottomSheetState,
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
