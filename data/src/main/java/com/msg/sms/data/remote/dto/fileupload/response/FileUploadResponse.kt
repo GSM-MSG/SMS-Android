f86cfdb75209d34b222bfdee420d34a195f2a8eb
khs3994 <z01039948003@gmail.com>
2023-05-23 00:10:30 +0900

    :sparkles: :: repository, model, module등록 작업 진행

--- data/src/main/java/com/msg/sms/data/remote/dto/fileupload/response/FileUploadResponse.kt
+++ data/src/main/java/com/msg/sms/data/remote/dto/fileupload/response/FileUploadResponse.kt
@@ -1,5 +1,12 @@
 package com.msg.sms.data.remote.dto.fileupload.response
 
+import com.msg.sms.domain.model.fileupload.response.FileUploadResponseModel
+
 data class FileUploadResponse(
     val fileUrl: String
 )
+
+fun FileUploadResponse.toFileUploadModel() =
+    FileUploadResponseModel(
+        fileUrl = this.fileUrl
+    )
