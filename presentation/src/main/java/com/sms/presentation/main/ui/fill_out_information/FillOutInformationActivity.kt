52ee32b65367ccb2f3f2d2173a7d0900c6fee161
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-23 16:13:36 +0900

    ✨ :: set Navigation

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
@@ -3,18 +3,23 @@ package com.sms.presentation.main.ui.fill_out_information
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class FillOutInformationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "Profile") {
+                composable("Profile") { ProfileScreen(navController = navController) }
+                composable("SchoolLife") { SchoolLifeScreen(navController = navController) }
+                composable("WorkCondition") { WorkConditionScreen(navController = navController) }
+                composable("MilitaryService") { MilitaryServiceScreen(navController = navController) }
+                composable("Certification") { CertificationScreen(navController = navController) }
+                composable("ForeignLanguage") { ForeignLanguageScreen(navController = navController) }
+            }
         }
     }
-}
-
-@Preview()
-@Composable
-fun DefaultPreview() {
 }
