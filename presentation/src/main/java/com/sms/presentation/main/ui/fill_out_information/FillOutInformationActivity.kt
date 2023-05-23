32f12cdfde612e21fad06410194aacc7b5fc9d01
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-23 14:17:18 +0900

    ✨ :: create FillOutInformation Activity

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
@@ -0,0 +1,20 @@
+package com.sms.presentation.main.ui.fill_out_information
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class FillOutInformationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun DefaultPreview() {
+}
