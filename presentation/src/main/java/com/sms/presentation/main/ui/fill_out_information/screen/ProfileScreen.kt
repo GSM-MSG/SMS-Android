91d2fb15181aac882ef4ba9d3f45bb3f05000349
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-23 23:15:27 +0900

    ✨ :: add parameter of viewModel

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/screen/ProfileScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/screen/ProfileScreen.kt
@@ -17,11 +17,13 @@ import com.msg.sms.design.component.spacer.SmsSpacer
 import com.msg.sms.design.component.topbar.TopBarComponent
 import com.msg.sms.design.icon.BackButtonIcon
 import com.sms.presentation.main.ui.fill_out_information.component.ProfileComponent
+import com.sms.presentation.main.viewmodel.StudentViewModel
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ProfileScreen(
     navController: NavController,
+    viewModel: StudentViewModel
 ) {
     val scrollState = rememberScrollState()
 
