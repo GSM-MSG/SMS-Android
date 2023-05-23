671cf4f2ff2bb0fef2cd238319d091dd52a2b8cd
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-23 16:15:43 +0900

    ✨ :: add button and parameter to navigating page

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/SchoolLifeScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/SchoolLifeScreen.kt
@@ -1,20 +1,23 @@
 package com.sms.presentation.main.ui.fill_out_information
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.msg.sms.design.component.button.ButtonState
+import com.msg.sms.design.component.button.SmsRoundedButton
 import com.msg.sms.design.component.spacer.SmsSpacer
 import com.msg.sms.design.component.topbar.TopBarComponent
 import com.msg.sms.design.icon.BackButtonIcon
 import com.msg.sms.design.theme.SMSTheme
 import com.sms.presentation.main.ui.fill_out_information.component.SchoolLifeComponent
 
-@Preview
 @Composable
-fun SchoolLifeScreen() {
+fun SchoolLifeScreen(
+    navController: NavController,
+) {
     SMSTheme { colors, _ ->
         Column(
             modifier = Modifier
@@ -25,7 +28,34 @@ fun SchoolLifeScreen() {
 
             }
             SmsSpacer()
-            SchoolLifeComponent(addDreamBook = {/* TODO(LeeHyeonbin) 드림북 파일 넣는 기능 추가하기 */})
+            SchoolLifeComponent(addDreamBook = {/* TODO(LeeHyeonbin) 드림북 파일 넣는 기능 추가하기 */ })
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)) {
+                Spacer(modifier = Modifier.weight(1f))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    SmsRoundedButton(
+                        text = "이전",
+                        modifier = Modifier
+                            .weight(2f)
+                            .height(48.dp),
+                        state = ButtonState.OutLine
+                    ) {
+                        navController.popBackStack()
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    SmsRoundedButton(
+                        text = "다음",
+                        modifier = Modifier
+                            .weight(4f)
+                            .height(48.dp),
+                        state = ButtonState.Normal
+                    ) {
+                        navController.navigate("WorkCondition")
+                    }
+                }
+                Spacer(modifier = Modifier.height(48.dp))
+            }
         }
     }
 }
