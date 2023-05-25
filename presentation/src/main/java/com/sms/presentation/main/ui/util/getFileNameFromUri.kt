05e02c264574d6f0af3c8e015e040d4a51702d3e
khs3994 <z01039948003@gmail.com>
2023-05-25 11:43:20 +0900

    :memo: :: 코멘트 반영

--- presentation/src/main/java/com/sms/presentation/main/ui/util/getFileNameFromUri.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/util/getFileNameFromUri.kt
@@ -1,18 +1,17 @@
 package com.sms.presentation.main.ui.util
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.provider.OpenableColumns
 
-@SuppressLint("Range")
 fun getFileNameFromUri(context: Context, uri: Uri): String? {
     var fileName: String? = null
     val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
     cursor?.let {
         if (it.moveToFirst()) {
-            val displayName = it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+            val columIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            val displayName = it.getString(columIndex)
             fileName = displayName ?: ""
         }
         cursor.close()
