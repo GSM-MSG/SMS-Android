f2e07a8be59c44d874967be8e13f2c82cf8dcc6c
khs3994 <z01039948003@gmail.com>
2023-05-25 09:25:12 +0900

    :fire: :: 불필요한 어노테이션 삭제

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/screen/SchoolLifeScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/screen/SchoolLifeScreen.kt
@@ -1,6 +1,5 @@
 package com.sms.presentation.main.ui.fill_out_information.screen
 
-import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -22,7 +21,6 @@ import com.msg.sms.design.theme.SMSTheme
 import com.sms.presentation.main.ui.fill_out_information.component.SchoolLifeComponent
 import com.sms.presentation.main.ui.util.getFileNameFromUri
 
-@SuppressLint("SuspiciousIndentation")
 @Composable
 fun SchoolLifeScreen(
     navController: NavController
@@ -43,48 +41,48 @@ fun SchoolLifeScreen(
     if (dreamBookFileUri.value != Uri.EMPTY && dreamBookFileUri.value != null)
         fileName.value = getFileNameFromUri(LocalContext.current, dreamBookFileUri.value)!!
 
-        SMSTheme { colors, _ ->
+    SMSTheme { colors, _ ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(colors.WHITE)
+        ) {
+            TopBarComponent(text = "정보입력", leftIcon = { BackButtonIcon() }, rightIcon = null) {
+
+            }
+            SmsSpacer()
+            SchoolLifeComponent(fileName = fileName.value) {
+                localStorageLauncher.launch("application/*")
+            }
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(colors.WHITE)
+                    .padding(horizontal = 20.dp)
             ) {
-                TopBarComponent(text = "정보입력", leftIcon = { BackButtonIcon() }, rightIcon = null) {
-
-                }
-                SmsSpacer()
-                SchoolLifeComponent(fileName = fileName.value) {
-                    localStorageLauncher.launch("application/*")
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        SmsRoundedButton(
-                            text = "이전",
-                            modifier = Modifier
-                                .weight(2f)
-                                .height(48.dp),
-                            state = ButtonState.OutLine
-                        ) {
-                            navController.popBackStack()
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
-                        SmsRoundedButton(
-                            text = "다음",
-                            modifier = Modifier
-                                .weight(4f)
-                                .height(48.dp),
-                            state = ButtonState.Normal
-                        ) {
-                            navController.navigate("WorkCondition")
-                        }
+                Spacer(modifier = Modifier.weight(1f))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    SmsRoundedButton(
+                        text = "이전",
+                        modifier = Modifier
+                            .weight(2f)
+                            .height(48.dp),
+                        state = ButtonState.OutLine
+                    ) {
+                        navController.popBackStack()
                     }
-                    Spacer(modifier = Modifier.height(48.dp))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    SmsRoundedButton(
+                        text = "다음",
+                        modifier = Modifier
+                            .weight(4f)
+                            .height(48.dp),
+                        state = ButtonState.Normal
+                    ) {
+                        navController.navigate("WorkCondition")
+                    }
                 }
+                Spacer(modifier = Modifier.height(48.dp))
             }
         }
+    }
 }
