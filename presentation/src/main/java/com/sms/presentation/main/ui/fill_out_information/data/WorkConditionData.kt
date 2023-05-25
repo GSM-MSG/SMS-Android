648fc3ca4b10960aa34a292d240ab17956d47695
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-25 09:42:36 +0900

    ✨ :: create workConditionDataClass

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/data/WorkConditionData.kt
@@ -0,0 +1,7 @@
+package com.sms.presentation.main.ui.fill_out_information.data
+
+data class WorkConditionData(
+    val formOfEmployment: String,
+    val salary: String,
+    val region: List<String>
+)
