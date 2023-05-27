b0caf6bfe3c33191b2a115deb9d9fc5c7fa4f72b
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-27 17:02:46 +0900

    ✨️ :: Write the get major logic that using useCase

--- presentation/src/main/java/com/sms/presentation/main/viewmodel/FillOutViewModel.kt
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/FillOutViewModel.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class StudentViewModel @Inject constructor(
+class FillOutViewModel @Inject constructor(
     private val enterStudentInformationUseCase: EnterStudentInformationUseCase,
     private val getMajorListUseCase: GetMajorListUseCase,
 ) : ViewModel() {
