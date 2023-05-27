40d7426de7a7ecb6b3e4701de62d17cb29f1e199
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-27 18:46:46 +0900

    ✨️ :: When onCreate request data to server

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
@@ -3,26 +3,34 @@ package com.sms.presentation.main.ui.fill_out_information
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sms.presentation.main.ui.fill_out_information.screen.*
+import com.sms.presentation.main.viewmodel.FillOutViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FillOutInformationActivity : ComponentActivity() {
 
+    private val viewModel by viewModels<FillOutViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.getMajorList()
+
         setContent {
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "Profile") {
+            NavHost(
+                navController = navController,
+                startDestination = "Profile"
+            ) {
                 composable("Profile") {
                     ProfileScreen(
                         navController = navController,
-                        viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
+                        viewModel = viewModel(LocalContext.current as FillOutInformationActivity),
                     )
                 }
                 composable("SchoolLife") {
@@ -57,5 +65,27 @@ class FillOutInformationActivity : ComponentActivity() {
                 }
             }
         }
+
+//        observeData()
+    }
+
+//    private fun observeData() {
+//        lifecycleScope.launch {
+//            viewModel.getMajorListEvent.observe(this@FillOutInformationActivity) { data ->
+//                when (data) {
+//                    is Event.Success -> {
+//                        showSuccessPage(data.data!!.major)
+//                    }
+//                    is Event.Loading -> {
+//                    }
+//                    else -> {
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    private fun showSuccessPage(list: List<String>) {
+
     }
 }
