f0992686f506b92f2febaf1ff72c8bd55c2c6742
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-05-28 16:50:20 +0900

    ✨️ :: add parameter and implement the save token logic

--- presentation/src/main/java/com/sms/presentation/main/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/AuthViewModel.kt
@@ -1,5 +1,6 @@
 package com.sms.presentation.main.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 import com.msg.sms.domain.model.auth.response.GAuthLoginResponseModel
 import com.msg.sms.domain.usecase.auth.GAuthLoginUseCase
+import com.msg.sms.domain.usecase.auth.SaveTheLoginDataUseCase
 import com.sms.presentation.main.viewmodel.util.Event
 import com.sms.presentation.main.viewmodel.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
-    private val gAuthLoginUseCase: GAuthLoginUseCase
+    private val gAuthLoginUseCase: GAuthLoginUseCase,
+    private val saveTheLoginDataUseCase: SaveTheLoginDataUseCase,
 ) : ViewModel() {
     private val _gAuthLoginRequest = MutableLiveData<Event<GAuthLoginResponseModel>>()
     val gAuthLoginRequest: LiveData<Event<GAuthLoginResponseModel>> get() = _gAuthLoginRequest
@@ -27,6 +30,16 @@ class AuthViewModel @Inject constructor(
             _gAuthLoginRequest.value = Event.Success(data = it)
         }.onFailure {
             _gAuthLoginRequest.value = it.errorHandling()
+        }
+    }
+
+    fun saveTheLoginData(data: GAuthLoginResponseModel) = viewModelScope.launch {
+        saveTheLoginDataUseCase(
+            data = data
+        ).onSuccess {
+
+        }.onFailure {
+            Log.d("TAG", "saveTheLoginData: $it")
         }
     }
 }
