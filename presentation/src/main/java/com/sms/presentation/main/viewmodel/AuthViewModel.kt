cbbf8d29869f063bae93e5028393cfef0e234e13
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-01 16:09:49 +0900

    ✨ :: auto login on LoginActivity

--- presentation/src/main/java/com/sms/presentation/main/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/AuthViewModel.kt
@@ -7,11 +7,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.sms.domain.model.auth.request.GAuthLoginRequestModel
 import com.msg.sms.domain.model.auth.response.GAuthLoginResponseModel
+import com.msg.sms.domain.model.major.MajorListModel
 import com.msg.sms.domain.usecase.auth.GAuthLoginUseCase
 import com.msg.sms.domain.usecase.auth.SaveTheLoginDataUseCase
+import com.msg.sms.domain.usecase.major.GetMajorListUseCase
 import com.sms.presentation.main.viewmodel.util.Event
 import com.sms.presentation.main.viewmodel.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,9 +22,13 @@ import javax.inject.Inject
 class AuthViewModel @Inject constructor(
     private val gAuthLoginUseCase: GAuthLoginUseCase,
     private val saveTheLoginDataUseCase: SaveTheLoginDataUseCase,
+    private val getMajorListUseCase: GetMajorListUseCase,
 ) : ViewModel() {
     private val _gAuthLoginRequest = MutableLiveData<Event<GAuthLoginResponseModel>>()
     val gAuthLoginRequest: LiveData<Event<GAuthLoginResponseModel>> get() = _gAuthLoginRequest
+
+    private val _getMajorList = MutableLiveData<Event<MajorListModel>>()
+    val getMajorList: LiveData<Event<MajorListModel>> get() = _getMajorList
 
     fun gAuthLogin(code: String) = viewModelScope.launch {
         gAuthLoginUseCase(
@@ -31,6 +38,19 @@ class AuthViewModel @Inject constructor(
         }.onFailure {
             _gAuthLoginRequest.value = it.errorHandling()
         }
+    }
+
+    fun getMajorList() = viewModelScope.launch {
+        getMajorListUseCase()
+            .onSuccess {
+                it.catch { remoteError ->
+                    _getMajorList.value = remoteError.errorHandling()
+                }.collect { response ->
+                    _getMajorList.value = Event.Success(data = response)
+                }
+            }.onFailure {
+                _getMajorList.value = it.errorHandling()
+            }
     }
 
     fun saveTheLoginData(data: GAuthLoginResponseModel) = viewModelScope.launch {
