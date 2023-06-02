7ab49707fc1c9b62da10c0ae03033281e37f607b
khs3994 <z01039948003@gmail.com>
2023-06-02 16:11:06 +0900

    :sparkles: :: Screen생성 및 nav에 등록

--- presentation/src/main/java/com/sms/presentation/main/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/main/MainActivity.kt
@@ -3,6 +3,12 @@ package com.sms.presentation.main.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.sms.presentation.main.ui.main.screen.MainScreen
+import com.sms.presentation.main.ui.main.screen.StudentFilteringScreen
+import com.sms.presentation.main.ui.main.screen.TectStackSearchScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +16,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "Main"
+            ) {
+                composable("Main") {
+                    MainScreen(
+                        navController = navController
+                    )
+                }
+                composable("StudentFiltering") {
+                    StudentFilteringScreen(
+                        navController = navController
+                    )
+                }
+                composable("TectStackSearch") {
+                    TectStackSearchScreen(
+                        navController = navController
+                    )
+                }
+            }
         }
     }
 }
---
+++ presentation/src/main/java/com/sms/presentation/main/ui/main/screen/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.sms.presentation.main.ui.main.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(
+    navController: NavController
+) {
+
+}
