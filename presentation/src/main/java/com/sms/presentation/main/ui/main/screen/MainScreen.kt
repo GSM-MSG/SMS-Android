e0e1a854bdf4aed396b632dc3870662476bec251
khs3994 <z01039948003@gmail.com>
2023-06-02 23:03:56 +0900

    :sparkles: :: list item 제작

--- presentation/src/main/java/com/sms/presentation/main/ui/main/screen/MainScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/main/screen/MainScreen.kt
@@ -1,23 +1,38 @@
 package com.sms.presentation.main.ui.main.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.sms.presentation.main.ui.main.component.MainScreenTopBar
+import com.sms.presentation.main.ui.main.component.StudentListItem
 
 @Composable
 fun MainScreen(
     navController: NavController
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
         MainScreenTopBar(
-            profileImageUrl = "https://",
+            profileImageUrl = "",
             filterButtonOnClick = { /*TODO (KimHyunseung) : 필터 Screen으로 이동*/ },
             profileButtonOnClick = { /*TODO (KimHyunseung) : 마이페이지로 이동*/ }
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        StudentListItem(
+            profileImageUrl = "",
+            major = "iOS Dev",
+            name = "최형우",
+            teckStackList = listOf("figma", "figma")
         )
     }
 }
