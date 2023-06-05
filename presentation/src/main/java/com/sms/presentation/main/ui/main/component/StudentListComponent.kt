58ed54932b69d2518e14ebc869ac95da5ed56568
khs3994 <z01039948003@gmail.com>
2023-06-05 16:41:45 +0900

    :memo: :: Spacer -> Divider

--- presentation/src/main/java/com/sms/presentation/main/ui/main/component/StudentListComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/main/component/StudentListComponent.kt
@@ -1,7 +1,12 @@
 package com.sms.presentation.main.ui.main.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.theme.SMSTheme
 import com.sms.presentation.main.ui.main.data.StudentData
 
 @Composable
@@ -9,19 +14,25 @@ fun StudentListComponent(
     studentList: List<StudentData>,
     onItemClick: () -> Unit
 ) {
-    LazyColumn {
-        items(studentList.size) {
-            with(studentList[it]) {
-                StudentListItem(
-                    profileImageUrl = profileImageUrl,
-                    major = major,
-                    name = name,
-                    teckStackList = teckStackList
-                ) {
-                    onItemClick()
+    SMSTheme { colors, _ ->
+        LazyColumn {
+            items(studentList.size) {
+                with(studentList[it]) {
+                    StudentListItem(
+                        profileImageUrl = profileImageUrl,
+                        major = major,
+                        name = name,
+                        teckStackList = teckStackList
+                    ) {
+                        onItemClick()
+                    }
                 }
+                Divider(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    color = colors.N10,
+                    thickness = 1.dp
+                )
             }
-            ListItemSpacer()
         }
     }
 }
