6ace07a61126e9bc316f060e9022cda0b610fc9d
khs3994 <z01039948003@gmail.com>
2023-06-09 08:51:45 +0900

    :memo: :: api명세서 업데이트에 따른 dto변경

--- domain/src/main/java/com/msg/sms/domain/model/student/response/StudentListModel.kt
+++ domain/src/main/java/com/msg/sms/domain/model/student/response/StudentListModel.kt
@@ -3,7 +3,7 @@ package com.msg.sms.domain.model.student.response
 data class StudentListModel(
     val content: List<StudentModel>,
     val page: Int,
-    val size: Int,
+    val contentSize: Int,
     val last: Boolean,
-    val isFilter: Boolean
+    val totalSize: Int
 )
