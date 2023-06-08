b214e0054f8bbe3d94a779b50fa9b10245d4e0ab
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-08 23:13:08 +0900

    ✨ :: create the SmsDivider

---
+++ design-system/src/main/java/com/msg/sms/design/component/divider/SmsDivider.kt
@@ -0,0 +1,18 @@
+package com.msg.sms.design.component.divider
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.theme.SMSTheme
+
+@Composable
+fun SmsDivider() {
+    SMSTheme { colors, _ ->
+        Divider(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp), color = colors.N20)
+    }
+}
