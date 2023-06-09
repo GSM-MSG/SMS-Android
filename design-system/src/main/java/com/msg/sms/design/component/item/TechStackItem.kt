8d8ba0f3c9fd01f44c1c8a7e7029d18d078cba9e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-09 10:24:18 +0900

    ✨ :: create the detail stack component

---
+++ design-system/src/main/java/com/msg/sms/design/component/item/TechStackItem.kt
@@ -0,0 +1,35 @@
+package com.msg.sms.design.component.item
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.theme.SMSTheme
+
+@Composable
+fun TechStackItem(techStack: String) {
+    SMSTheme { colors, typography ->
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(colors.N10)
+        ) {
+            Text(
+                text = techStack,
+                style = typography.caption2,
+                fontWeight = FontWeight.Normal,
+                color = colors.N40,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(vertical = 6.5.dp, horizontal = 12.dp)
+            )
+        }
+    }
+}
