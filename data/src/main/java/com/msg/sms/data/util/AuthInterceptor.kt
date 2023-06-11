380cf3109cf2d9a63c795010ee66ee48d2abd60a
khs3994 <z01039948003@gmail.com>
2023-06-12 01:19:02 +0900

    :memo: :: 인터셉터에서 토큰 관련 정보에 " 이 포함되어 있을 경우 삭제하도록 처리

--- data/src/main/java/com/msg/sms/data/util/AuthInterceptor.kt
+++ data/src/main/java/com/msg/sms/data/util/AuthInterceptor.kt
@@ -28,8 +28,8 @@ class AuthInterceptor @Inject constructor(
             }
         }
         runBlocking {
-            val refreshTime = dataSource.getRefreshTime().first()
-            val accessTime = dataSource.getAccessTime().first()
+            val refreshTime = dataSource.getRefreshTime().first().replace("\"", "")
+            val accessTime = dataSource.getAccessTime().first().replace("\"", "")
 
             if (refreshTime == "" || currentTime.after(refreshTime.toDate())) throw NeedLoginException()
 //            access 토큰 재 발급
@@ -38,7 +38,7 @@ class AuthInterceptor @Inject constructor(
                 val refreshRequest = Request.Builder()
                     .url(BuildConfig.BASE_URL + "auth")
                     .patch(chain.request().body ?: RequestBody.create(null, byteArrayOf()))
-                    .addHeader("Refresh-Token", dataSource.getRefreshToken().first())
+                    .addHeader("Refresh-Token", dataSource.getRefreshToken().first().replace("\"",""))
                     .build()
                 val jsonParser = JsonParser()
                 val response = client.newCall(refreshRequest).execute()
@@ -50,7 +50,7 @@ class AuthInterceptor @Inject constructor(
                     dataSource.setRefreshTime(token["refreshTokenExp"].toString())
                 } else throw NeedLoginException()
             }
-            val accessToken = dataSource.getAccessToken().first()
+            val accessToken = dataSource.getAccessToken().first().replace("\"", "")
             builder.addHeader("Authorization", "Bearer $accessToken")
         }
         return chain.proceed(builder.build())
