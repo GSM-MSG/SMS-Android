adeaf30f50fd69dcfd5ca97c524d3b083eb2b559
khs3994 <z01039948003@gmail.com>
2023-06-12 08:10:42 +0900

    :memo: :: add todo

--- presentation/src/main/java/com/sms/presentation/main/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/login/LoginActivity.kt
@@ -48,7 +48,7 @@ class LoginActivity : ComponentActivity() {
 
     private fun observeEvent() {
         observeLoginEvent()
-        observeAutoLoginCheck()
+        //observeAutoLoginCheck() //TODO (Kimhyunseung) - 자동로그인 조건 변경 필요
         observeSaveTokenEvent()
     }
 
