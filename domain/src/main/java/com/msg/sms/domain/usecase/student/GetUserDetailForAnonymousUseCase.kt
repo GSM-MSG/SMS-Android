5461758122514a7a63d35b410be6d4f01ace5f2b
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-12 23:31:13 +0900

    ✨ :: create useCase

---
+++ domain/src/main/java/com/msg/sms/domain/usecase/student/GetUserDetailForAnonymousUseCase.kt
@@ -0,0 +1,13 @@
+package com.msg.sms.domain.usecase.student
+
+import com.msg.sms.domain.repository.StudentRepository
+import java.util.UUID
+import javax.inject.Inject
+
+class GetUserDetailForAnonymousUseCase @Inject constructor(
+    private val repository: StudentRepository,
+) {
+    suspend operator fun invoke(uuid: UUID) = kotlin.runCatching {
+        repository.getUserDetailForAnonymous(uuid = uuid)
+    }
+}
