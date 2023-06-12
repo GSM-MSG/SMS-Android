dd2065ae1ac80d4844eedb57fa96c5569cc51534
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-12 23:30:52 +0900

    ✨ :: create dataSourceImpl

--- data/src/main/java/com/msg/sms/data/repository/StudentRepositoryImpl.kt
+++ data/src/main/java/com/msg/sms/data/repository/StudentRepositoryImpl.kt
@@ -3,16 +3,23 @@ package com.msg.sms.data.repository
 import com.msg.sms.data.remote.datasource.student.RemoteStudentDataSource
 import com.msg.sms.data.remote.dto.student.request.CertificateInformation
 import com.msg.sms.data.remote.dto.student.request.EnterStudentInformationRequest
+import com.msg.sms.data.remote.dto.student.response.toGetStudentForAnonymous
+import com.msg.sms.data.remote.dto.student.response.toGetStudentForStudent
+import com.msg.sms.data.remote.dto.student.response.toGetStudentForTeacher
 import com.msg.sms.data.remote.dto.student.response.toStudentListModel
 import com.msg.sms.domain.model.student.request.EnterStudentInformationModel
+import com.msg.sms.domain.model.student.response.GetStudentForAnonymous
+import com.msg.sms.domain.model.student.response.GetStudentForStudent
+import com.msg.sms.domain.model.student.response.GetStudentForTeacher
 import com.msg.sms.domain.model.student.response.StudentListModel
 import com.msg.sms.domain.repository.StudentRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.util.*
 import javax.inject.Inject
 
 class StudentRepositoryImpl @Inject constructor(
-    private val dataSource: RemoteStudentDataSource
+    private val dataSource: RemoteStudentDataSource,
 ) : StudentRepository {
     override suspend fun enterStudentInformation(body: EnterStudentInformationModel): Flow<Unit> {
         return dataSource.enterStudentInformation(
@@ -55,7 +62,7 @@ class StudentRepositoryImpl @Inject constructor(
         minSalary: Int?,
         maxSalary: Int?,
         gsmAuthenticationScoreSort: String?,
-        salarySort: String?
+        salarySort: String?,
     ): Flow<StudentListModel> {
         return dataSource.getStudentList(
             page = page,
@@ -74,5 +81,18 @@ class StudentRepositoryImpl @Inject constructor(
             gsmAuthenticationScoreSort = gsmAuthenticationScoreSort,
             salarySort = salarySort
         ).map { it.toStudentListModel() }
+    }
+
+    override suspend fun getUserDetailForStudent(uuid: UUID): Flow<GetStudentForStudent> {
+        return dataSource.getUserDetailForStudent(uuid = uuid).map { it.toGetStudentForStudent() }
+    }
+
+    override suspend fun getUserDetailForAnonymous(uuid: UUID): Flow<GetStudentForAnonymous> {
+        return dataSource.getUserDetailForAnonymous(uuid = uuid)
+            .map { it.toGetStudentForAnonymous() }
+    }
+
+    override suspend fun getUserDetailForTeacher(uuid: UUID): Flow<GetStudentForTeacher> {
+        return dataSource.getUserDetailForTeacher(uuid = uuid).map { it.toGetStudentForTeacher() }
     }
 }
