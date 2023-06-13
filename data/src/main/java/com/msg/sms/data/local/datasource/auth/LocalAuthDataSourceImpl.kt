562aece319c86ed499b4c4968e6a66b3e079d94f
khs3994 <z01039948003@gmail.com>
2023-06-14 01:00:20 +0900

    :sparkles: :: dataStore접근 로직

--- data/src/main/java/com/msg/sms/data/local/datasource/auth/LocalAuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/sms/data/local/datasource/auth/LocalAuthDataSourceImpl.kt
@@ -84,4 +84,10 @@ class LocalAuthDataSourceImpl @Inject constructor(
             it[AuthPreferenceKey.ROLE] = role
         }
     }
+
+    override suspend fun deleteRoleInfo() {
+        dataStore.edit {
+            it.remove(AuthPreferenceKey.ROLE)
+        }
+    }
 }
