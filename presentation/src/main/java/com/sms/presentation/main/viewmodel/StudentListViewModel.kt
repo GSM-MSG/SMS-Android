08da09712543e0692ba719e05f6025b1873794d9
khs3994 <z01039948003@gmail.com>
2023-06-13 12:05:07 +0900

    :sparkles: :: detail받는 뷰모델 작성

--- presentation/src/main/java/com/sms/presentation/main/viewmodel/StudentListViewModel.kt
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/StudentListViewModel.kt
@@ -2,8 +2,14 @@ package com.sms.presentation.main.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.msg.sms.domain.model.student.response.GetStudentForAnonymous
+import com.msg.sms.domain.model.student.response.GetStudentForStudent
+import com.msg.sms.domain.model.student.response.GetStudentForTeacher
 import com.msg.sms.domain.model.student.response.StudentListModel
+import com.msg.sms.domain.usecase.student.GetStudentDetailForStudentUseCase
 import com.msg.sms.domain.usecase.student.GetStudentListUseCase
+import com.msg.sms.domain.usecase.student.GetUserDetailForAnonymousUseCase
+import com.msg.sms.domain.usecase.student.GetUserDetailForTeacherUseCase
 import com.sms.presentation.main.viewmodel.util.Event
 import com.sms.presentation.main.viewmodel.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,14 +17,30 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class StudentListViewModel @Inject constructor(
-    private val getStudentListUseCase: GetStudentListUseCase
+    private val getStudentListUseCase: GetStudentListUseCase,
+    private val getStudentDetailForTeacherUseCase: GetUserDetailForTeacherUseCase,
+    private val getStudentDetailForStudentUseCase: GetStudentDetailForStudentUseCase,
+    private val getStudentDetailForAnonymousUseCase: GetUserDetailForAnonymousUseCase
 ) : ViewModel() {
     private val _getStudentListResponse = MutableStateFlow<Event<StudentListModel>>(Event.Loading)
     val getStudentListResponse = _getStudentListResponse.asStateFlow()
+
+    private val _getStudentDetailForTeacherResponse =
+        MutableStateFlow<Event<GetStudentForTeacher>>(Event.Loading)
+    val getStudentDetailForTeacherResponse = _getStudentDetailForTeacherResponse.asStateFlow()
+
+    private val _getStudentDetailForStudentResponse =
+        MutableStateFlow<Event<GetStudentForStudent>>(Event.Loading)
+    val getStudentDetailForStudentResponse = _getStudentDetailForStudentResponse.asStateFlow()
+
+    private val _getStudentDetailForAnonymousResponse =
+        MutableStateFlow<Event<GetStudentForAnonymous>>(Event.Loading)
+    val getStudentDetailForAnonymousResponse = _getStudentDetailForAnonymousResponse.asStateFlow()
 
     fun getStudentListRequest(
         page: Int,
@@ -64,4 +86,47 @@ class StudentListViewModel @Inject constructor(
             _getStudentListResponse.value = error.errorHandling()
         }
     }
+
+    fun getStudentDetailForTeacher(uuid: UUID) = viewModelScope.launch {
+        getStudentDetailForTeacherUseCase(
+            uuid = uuid
+        ).onSuccess {
+            it.catch { remoteError ->
+                _getStudentDetailForTeacherResponse.value = remoteError.errorHandling()
+            }.collect { response ->
+                _getStudentDetailForTeacherResponse.value = Event.Success(data = response)
+            }
+        }.onFailure { error ->
+            _getStudentDetailForTeacherResponse.value = error.errorHandling()
+        }
+    }
+
+    fun getStudentDetailForStudent(uuid: UUID) = viewModelScope.launch {
+        getStudentDetailForStudentUseCase(
+            uuid = uuid
+        ).onSuccess {
+            it.catch { remoteError ->
+                _getStudentDetailForStudentResponse.value = remoteError.errorHandling()
+            }.collect { response ->
+                _getStudentDetailForStudentResponse.value = Event.Success(data = response)
+            }
+        }.onFailure { error ->
+            _getStudentDetailForStudentResponse.value = error.errorHandling()
+        }
+    }
+
+    fun getStudentDetailForAnonymous(uuid: UUID) = viewModelScope.launch {
+        getStudentDetailForAnonymousUseCase(
+            uuid = uuid
+        ).onSuccess {
+            it.catch { remoteError ->
+                _getStudentDetailForAnonymousResponse.value = remoteError.errorHandling()
+            }.collect { response ->
+                _getStudentDetailForAnonymousResponse.value = Event.Success(data = response)
+            }
+        }.onFailure { error ->
+            _getStudentDetailForAnonymousResponse.value = error.errorHandling()
+        }
+    }
+
 }
