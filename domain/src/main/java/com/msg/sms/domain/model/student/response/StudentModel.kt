9b36efc961d86f93d099b78821b9243b199bbda5
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-13 09:24:19 +0900

    ✨ :: Add UUID on parameter

--- domain/src/main/java/com/msg/sms/domain/model/student/response/StudentModel.kt
+++ domain/src/main/java/com/msg/sms/domain/model/student/response/StudentModel.kt
@@ -1,6 +1,9 @@
 package com.msg.sms.domain.model.student.response
 
+import java.util.UUID
+
 data class StudentModel(
+    val id: UUID,
     val profileImg: String,
     val name: String,
     val major: String,
