3794c5cd896987d05b488b5ccdb088c93df29218
khs3994 <z01039948003@gmail.com>
2023-06-13 20:44:09 +0900

    :memo: :: 띄어쓰기 제거

--- presentation/src/main/java/com/sms/presentation/main/ui/detail/StudentDetailScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/detail/StudentDetailScreen.kt
@@ -89,7 +89,7 @@ fun StudentDetailScreen(
                 val downloader = AndroidDownloader(
                     context = context,
                     fileName =
-                    "${studentDetailData.grade}${studentDetailData.classNum}${studentDetailData.number}${studentDetailData.name} 의 드림북"
+                    "${studentDetailData.grade}${studentDetailData.classNum}${studentDetailData.number}${studentDetailData.name}의 드림북"
                 )
                 downloader.downloadFile(url = studentDetailData.dreamBookFileUrl!!)
             },
