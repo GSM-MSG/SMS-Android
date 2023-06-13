10822156600d2d99605fbc3d65cd688870b6b4b1
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-06-13 12:09:29 +0900

    ✨️ :: create AndroidDownloader.kt

---
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/util/downloader/AndroidDownloader.kt
@@ -0,0 +1,25 @@
+package com.sms.presentation.main.viewmodel.util.downloader
+
+import android.app.DownloadManager
+import android.content.Context
+import android.os.Environment
+import androidx.core.net.toUri
+
+class AndroidDownloader(
+    private val context: Context,
+    private val fileName: String
+) : Downloader {
+
+    private val downloadManager = context.getSystemService(DownloadManager::class.java)
+
+    override fun downloadFile(url: String): Long {
+        val request = DownloadManager.Request(url.toUri())
+            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setTitle(fileName)
+            .setDescription("f$fileName 다운 중...")
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+        return downloadManager.enqueue(request)
+    }
+
+}
