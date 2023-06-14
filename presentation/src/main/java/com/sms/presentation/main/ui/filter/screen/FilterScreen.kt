30d35a2573918f9463f5c854b17a0fd5b4092292
khs3994 <z01039948003@gmail.com>
2023-06-14 23:07:41 +0900

    :sparkles: :: add top bar

--- presentation/src/main/java/com/sms/presentation/main/ui/filter/screen/FilterScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/filter/screen/FilterScreen.kt
@@ -1,7 +1,18 @@
 package com.sms.presentation.main.ui.filter.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.msg.sms.design.component.divider.SmsDivider
+import com.msg.sms.design.component.topbar.TopBarComponent
+import com.msg.sms.design.icon.DeleteButtonIcon
+import com.msg.sms.design.theme.SMSTheme
 import com.sms.presentation.main.viewmodel.StudentListViewModel
 import kotlinx.coroutines.CoroutineScope
 
@@ -12,5 +23,51 @@ fun FilterScreen(
     lifecycleScope: CoroutineScope,
     role: String
 ) {
+    SMSTheme { colors, typography ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(colors.WHITE)
+        ) {
+            TopBarComponent(
+                text = "필터",
+                leftIcon = {
+                    Text(
+                        text = "초기화",
+                        style = typography.body2,
+                        fontWeight = FontWeight.Normal,
+                        color = colors.BLACK
+                    )
+                },
+                rightIcon = { DeleteButtonIcon() }
+            )
+            SmsDivider()
+        }
+    }
+}
 
+@Preview
+@Composable
+fun FilterScreenPre() {
+    SMSTheme { colors, typography ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(colors.WHITE)
+        ) {
+            TopBarComponent(
+                text = "필터",
+                leftIcon = {
+                    Text(
+                        text = "초기화",
+                        style = typography.body2,
+                        fontWeight = FontWeight.Normal,
+                        color = colors.BLACK
+                    )
+                },
+                rightIcon = { DeleteButtonIcon() }
+            )
+            SmsDivider()
+        }
+    }
 }
