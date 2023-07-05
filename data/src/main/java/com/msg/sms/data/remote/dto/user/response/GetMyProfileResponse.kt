adf2ce504efb9e7bf25e20929c2661533d05670a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-07-05 21:00:52 +0900

    ✨ :: create my profile data class of data response

---
+++ data/src/main/java/com/msg/sms/data/remote/dto/user/response/GetMyProfileResponse.kt
@@ -0,0 +1,75 @@
+package com.msg.sms.data.remote.dto.user.response
+
+import com.google.gson.annotations.SerializedName
+import com.msg.sms.domain.model.user.response.LanguageCertificateModel
+import com.msg.sms.domain.model.user.response.MyProfileModel
+
+data class GetMyProfileResponse(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("introduce")
+    val introduce: String,
+    @SerializedName("dreamBookFileUrl")
+    val dreamBookFileUrl: String,
+    @SerializedName("portfolioUrl")
+    val portfolioUrl: String,
+    @SerializedName("grade")
+    val grade: Int,
+    @SerializedName("classNum")
+    val classNum: Int,
+    @SerializedName("number")
+    val number: Int,
+    @SerializedName("department")
+    val department: String,
+    @SerializedName("major")
+    val major: String,
+    @SerializedName("profileImg")
+    val profileImg: String,
+    @SerializedName("contactEmail")
+    val contactEmail: String,
+    @SerializedName("gsmAuthenticationScore")
+    val gsmAuthenticationScore: Int,
+    @SerializedName("formOfEmployment")
+    val formOfEmployment: String,
+    @SerializedName("regions")
+    val regions: List<String>,
+    @SerializedName("militaryService")
+    val militaryService: String,
+    @SerializedName("salary")
+    val salary: Int,
+    @SerializedName("languageCertificates")
+    val languageCertificates: List<LanguageCertificateResponse>,
+    @SerializedName("certificates")
+    val certificates: List<String>,
+    @SerializedName("techStacks")
+    val techStacks: List<String>,
+)
+
+fun GetMyProfileResponse.toMyProfileModel(): MyProfileModel {
+    return MyProfileModel(
+        name = this.name,
+        introduce = this.introduce,
+        dreamBookFileUrl = this.dreamBookFileUrl,
+        portfolioUrl = this.portfolioUrl,
+        grade = this.grade,
+        classNum = this.classNum,
+        number = this.number,
+        department = this.department,
+        major = this.major,
+        profileImg = this.profileImg,
+        contactEmail = this.contactEmail,
+        gsmAuthenticationScore = this.gsmAuthenticationScore,
+        formOfEmployment = this.formOfEmployment,
+        regions = this.regions,
+        militaryService = this.militaryService,
+        salary = this.salary,
+        languageCertificates = this.languageCertificates.map {
+            LanguageCertificateModel(
+                languageCertificateName = it.languageCertificateName,
+                score = it.score
+            )
+        },
+        certificates = this.certificates,
+        techStacks = this.techStacks
+    )
+}
