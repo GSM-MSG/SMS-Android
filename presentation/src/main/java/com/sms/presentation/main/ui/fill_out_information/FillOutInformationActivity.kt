7030890c443c5e64d1e60b8fa0772edd34202951
khs3994 <z01039948003@gmail.com>
2023-08-01 17:45:35 +0900

    :sparkles: :: status bar component 제작

--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/FillOutInformationActivity.kt
@@ -2,9 +2,12 @@ package com.sms.presentation.main.ui.fill_out_information
 
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -14,8 +17,10 @@ import androidx.navigation.compose.rememberNavController
 import com.msg.sms.design.component.spacer.SmsSpacer
 import com.msg.sms.design.component.topbar.TopBarComponent
 import com.msg.sms.design.icon.BackButtonIcon
+import com.msg.sms.design.theme.SMSTheme
 import com.sms.presentation.main.ui.base.BaseActivity
 import com.sms.presentation.main.ui.detail_stack_search.DetailStackSearchScreen
+import com.sms.presentation.main.ui.fill_out_information.component.FilloutStatusProgressBar
 import com.sms.presentation.main.ui.fill_out_information.screen.*
 import com.sms.presentation.main.viewmodel.FillOutViewModel
 import com.sms.presentation.main.viewmodel.SearchDetailStackViewModel
@@ -32,82 +37,96 @@ class FillOutInformationActivity : BaseActivity() {
 
         setContent {
             val navController = rememberNavController()
-            Column {
-                TopBarComponent(text = "정보 입력", leftIcon = { BackButtonIcon() }, rightIcon = null) {
-                    navController.popBackStack()
-                }
-                SmsSpacer()
-                NavHost(
-                    navController = navController,
-                    startDestination = "Profile"
+            val currentNavBackStackSize = remember {
+                mutableStateOf(navController.backQueue.size)
+            }
+            SMSTheme { colors, _ ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(colors.WHITE)
                 ) {
-                    composable("Profile") {
-                        setSoftInputMode("PAN")
-                        val data = remember {
-                            mutableStateOf(
-                                navController.previousBackStackEntry?.savedStateHandle?.get<String>(
-                                    "detailStack"
+                    TopBarComponent(
+                        text = "정보 입력",
+                        leftIcon = { BackButtonIcon() },
+                        rightIcon = null
+                    ) {
+                        navController.popBackStack()
+                    }
+                    FilloutStatusProgressBar(currentNavBackStackSize.value)
+                    SmsSpacer()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "Profile"
+                    ) {
+                        composable("Profile") {
+                            setSoftInputMode("PAN")
+                            val data = remember {
+                                mutableStateOf(
+                                    navController.previousBackStackEntry?.savedStateHandle?.get<String>(
+                                        "detailStack"
+                                    )
                                 )
+                            }
+                            ProfileScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity),
+                                detailStack = (if (data.value != null) data.value!!.split(",") else listOf())
                             )
                         }
-                        ProfileScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity),
-                            detailStack = (if (data.value != null) data.value!!.split(",") else listOf())
-                        )
-                    }
-                    composable("SchoolLife") {
-                        setSoftInputMode()
-                        SchoolLifeScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
-                        )
-                    }
-                    composable("WorkCondition") {
-                        setSoftInputMode("PAN")
-                        WorkConditionScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
-                        )
-                    }
-                    composable("MilitaryService") {
-                        setSoftInputMode()
-                        MilitaryServiceScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
-                        )
-                    }
-                    composable("Certification") {
-                        setSoftInputMode("PAN")
-                        CertificationScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
-                        )
-                    }
-                    composable("ForeignLanguage") {
-                        setSoftInputMode("PAN")
-                        ForeignLanguageScreen(
-                            navController = navController,
-                            viewModel = viewModel(LocalContext.current as FillOutInformationActivity),
-                            lifecycleScope = lifecycleScope
-                        )
-                    }
+                        composable("SchoolLife") {
+                            setSoftInputMode()
+                            SchoolLifeScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
+                            )
+                        }
+                        composable("WorkCondition") {
+                            setSoftInputMode("PAN")
+                            WorkConditionScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
+                            )
+                        }
+                        composable("MilitaryService") {
+                            setSoftInputMode()
+                            MilitaryServiceScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
+                            )
+                        }
+                        composable("Certification") {
+                            setSoftInputMode("PAN")
+                            CertificationScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity)
+                            )
+                        }
+                        composable("ForeignLanguage") {
+                            setSoftInputMode("PAN")
+                            ForeignLanguageScreen(
+                                navController = navController,
+                                viewModel = viewModel(LocalContext.current as FillOutInformationActivity),
+                                lifecycleScope = lifecycleScope
+                            )
+                        }
 
-                    composable("Search") {
-                        setSoftInputMode("RESIZE")
-                        val data = remember {
-                            mutableStateOf(
-                                navController.previousBackStackEntry?.savedStateHandle?.get<String>(
-                                    "detailStack"
+                        composable("Search") {
+                            setSoftInputMode("RESIZE")
+                            val data = remember {
+                                mutableStateOf(
+                                    navController.previousBackStackEntry?.savedStateHandle?.get<String>(
+                                        "detailStack"
+                                    )
                                 )
-                            )
-                        }
-                        DetailStackSearchScreen(
-                            navController = navController,
-                            viewModel = searchDetailStackViewModel,
-                            selectedStack = (if (data.value != null) data.value!!.split(",") else listOf())
-                        ) {
-                            navController.navigate("Profile")
+                            }
+                            DetailStackSearchScreen(
+                                navController = navController,
+                                viewModel = searchDetailStackViewModel,
+                                selectedStack = (if (data.value != null) data.value!!.split(",") else listOf())
+                            ) {
+                                navController.navigate("Profile")
+                            }
                         }
                     }
                 }
---
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/component/FilloutStatusProgressBar.kt
@@ -0,0 +1,82 @@
+package com.sms.presentation.main.ui.fill_out_information.component
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.theme.SMSTheme
+
+@Composable
+fun FilloutStatusProgressBar(progressCount: Int) {
+    var progress by remember { mutableStateOf(0f) }
+
+    when (progressCount) {
+        0 -> progress = 0.025f
+        1 -> progress = 0.25f
+        2 -> progress = 0.375f
+        3 -> progress = 0.5f
+        4 -> progress = 0.625f
+        5 -> progress = 0.75f
+        6 -> progress = 0.875f
+        7 -> progress = 0.1f
+    }
+
+    val size by animateFloatAsState(
+        targetValue = progress,
+        tween(
+            durationMillis = 1000,
+            delayMillis = 200,
+            easing = LinearOutSlowInEasing
+        )
+    )
+
+    SMSTheme { colors, _ ->
+        Box(modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clip(RoundedCornerShape(9.dp))
+                    .background(colors.N10)
+                    .align(Alignment.CenterStart),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                (1..8).forEach { _ ->
+                    Box(
+                        modifier = Modifier
+                            .size(12.dp)
+                            .padding(3.dp)
+                            .clip(CircleShape)
+                            .background(colors.N20)
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(size)
+                    .height(9.dp)
+                    .clip(RoundedCornerShape(9.dp))
+                    .background(colors.P2)
+                    .animateContentSize()
+                    .align(Alignment.CenterStart)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CustomProgressBarPre() {
+    FilloutStatusProgressBar(progressCount = 2)
+}
