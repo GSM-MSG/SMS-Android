17960cb8d159a987b1d78e3e4b3d463633a24abf
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-02 13:27:49 +0900

    🔥 :: Remove the unused import

--- presentation/src/main/java/com/sms/presentation/main/ui/detail/link/LinkComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/detail/link/LinkComponent.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
