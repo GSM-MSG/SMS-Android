3dc772414feb63e42873a788691c65805a2402d4
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-05 23:03:34 +0900

    ✨ ::  Create the withdrawal button component

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/button/WithdrawalButton.kt
@@ -0,0 +1,46 @@
+package com.sms.presentation.main.ui.mypage.component.button
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.msg.sms.design.icon.RedWithdrawalIcon
+import com.msg.sms.design.modifier.smsClickable
+import com.msg.sms.design.theme.SMSTheme
+
+@Composable
+fun WithdrawalButton(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .smsClickable(onClick = onClick),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RedWithdrawalIcon(modifier = Modifier.padding(start = 12.dp, top = 13.5.dp, bottom = 13.5.dp))
+        SMSTheme { colors, typography ->
+            Text(
+                text = "회원탈퇴",
+                style = typography.title2,
+                fontWeight = FontWeight.Bold,
+                color = colors.ERROR
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WithdrawalButtonPre() {
+    WithdrawalButton(onClick = {})
+}
