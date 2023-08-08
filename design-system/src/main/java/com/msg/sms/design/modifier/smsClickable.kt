9bdf23c7413a2229100a3f0e7ba00a2610830ff8
khs3994 <z01039948003@gmail.com>
2023-08-08 20:43:36 +0900

    :lipstick: :: smsClickable 디폴트 값 변경

--- design-system/src/main/java/com/msg/sms/design/modifier/smsClickable.kt
+++ design-system/src/main/java/com/msg/sms/design/modifier/smsClickable.kt
@@ -3,20 +3,21 @@ package com.msg.sms.design.modifier
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
 fun Modifier.smsClickable(
     rippleEnabled: Boolean = true,
     bounded: Boolean = true,
-    radius: Dp = 15.dp,
+    radius: Dp = Dp.Unspecified,
     onClick: () -> Unit
 ) = composed {
+    this.wrapContentSize()
     combinedClickable(
         onClick = onClick,
         interactionSource = remember { MutableInteractionSource() },
--- presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/component/ProjectPreviewInputComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_information/component/ProjectPreviewInputComponent.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
