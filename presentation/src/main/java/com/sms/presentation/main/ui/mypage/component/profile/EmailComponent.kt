e24638209067ce13ab7bd8651ea3e3f435fcc4a6
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-09 16:11:04 +0900

    ✨ :: Set the email text field

--- presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/EmailComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/EmailComponent.kt
@@ -2,20 +2,25 @@ package com.sms.presentation.main.ui.mypage.component.profile
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.msg.sms.design.component.textfield.SmsTextField
 import com.msg.sms.design.util.AddGrayBody1Title
 
 @Composable
-fun EmailComponent() {
+fun EmailComponent(emailValue: String) {
+    val email = remember {
+        mutableStateOf(emailValue)
+    }
     AddGrayBody1Title(titleText = "이메일") {
         SmsTextField(
             modifier = Modifier.fillMaxWidth(),
-            setText = "",
+            setText = email.value,
             placeHolder = "이메일",
-            onValueChange = {}) {
-
+            onValueChange = {email.value = it}) {
+            email.value = ""
         }
     }
 }
@@ -23,5 +28,5 @@ fun EmailComponent() {
 @Preview
 @Composable
 private fun EmailComponentPre() {
-    EmailComponent()
+    EmailComponent("s21042@gsm.hs.kr")
 }
