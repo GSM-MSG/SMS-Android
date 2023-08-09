faa2f0ae4649a2d6d12608a5dec584e3ee203b2e
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-09 17:22:53 +0900

    ✨ :: FillOut the parameter values

--- presentation/src/main/java/com/sms/presentation/main/ui/mypage/section/ProfileSection.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/section/ProfileSection.kt
@@ -16,18 +16,23 @@ import com.sms.presentation.main.ui.mypage.component.profile.PortfolioComponent
 import com.sms.presentation.main.ui.mypage.component.profile.SelfIntroduceComponent
 
 @Composable
-fun ProfileSection() {
+fun ProfileSection(setMajor: String, onClickMajorComponent: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 24.dp, start = 20.dp, end = 20.dp, bottom = 20.dp),
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
-        PicturePickerComponent()
-        SelfIntroduceComponent()
-        EmailComponent()
-        MajorComponent()
-        PortfolioComponent()
+        PicturePickerComponent(imageUrl = "https://avatars.githubusercontent.com/u/82383983?s=400&u=776e1d000088224cbabf4dec2bdea03071aaaef2&v=4")
+        SelfIntroduceComponent(introduceValue = "안드로이드의 왕이 될 남자")
+        EmailComponent(emailValue = "s21042@gsm.hs.kr")
+        MajorComponent(
+            majorValue = "Android",
+            isSelfTyping = setMajor == "직접입력",
+            onClick = onClickMajorComponent,
+            setMajorText = setMajor
+        )
+        PortfolioComponent(portfolioValue = "https://youtube.com")
         DetailTechStackComponent(addedList = listOf("AndroidStdio", "Kotlin", "Flutter"))
     }
 }
@@ -35,5 +40,5 @@ fun ProfileSection() {
 @Preview
 @Composable
 private fun ProfileSectionPre() {
-    ProfileSection()
+    ProfileSection(setMajor = "Android", onClickMajorComponent = {})
 }
