ad199a55a7350077c3ebf4f6958c1f56ebc15ea4
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-09 16:11:32 +0900

    ✨ :: Set introduce Text field

--- presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/IntroduceComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/IntroduceComponent.kt
@@ -2,20 +2,27 @@ package com.sms.presentation.main.ui.mypage.component.profile
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.msg.sms.design.component.textfield.SmsTextField
 import com.msg.sms.design.util.AddGrayBody1Title
 
 @Composable
-fun SelfIntroduceComponent() {
-    AddGrayBody1Title(titleText = "자기소개") {
+fun SelfIntroduceComponent(introduceValue: String) {
+    AddGrayBody1Title(titleText = "자기 소개") {
+        val introduce = remember {
+            mutableStateOf(introduceValue)
+        }
         SmsTextField(
             modifier = Modifier.fillMaxWidth(),
-            setText = "",
-            placeHolder = "1줄 자기소개 입력",
-            onValueChange = {}) {
-
+            setText = introduce.value,
+            placeHolder = "1줄 자기 소개 입력",
+            onValueChange = {
+                introduce.value = it
+            }) {
+            introduce.value = ""
         }
     }
 }
@@ -23,5 +30,5 @@ fun SelfIntroduceComponent() {
 @Preview
 @Composable
 private fun SelfIntroduceComponentPre() {
-    SelfIntroduceComponent()
+    SelfIntroduceComponent("Will be king of Android")
 }
