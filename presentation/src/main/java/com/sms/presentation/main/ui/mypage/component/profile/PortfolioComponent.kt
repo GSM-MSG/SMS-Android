731991540c5a5c508c318e110b5cbf5419c5bd4f
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-09 17:21:52 +0900

    ✨ :: Implement the portfolio text field

--- presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/PortfolioComponent.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/component/profile/PortfolioComponent.kt
@@ -2,20 +2,25 @@ package com.sms.presentation.main.ui.mypage.component.profile
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.msg.sms.design.component.textfield.SmsTextField
 import com.msg.sms.design.util.AddGrayBody1Title
 
 @Composable
-fun PortfolioComponent() {
+fun PortfolioComponent(portfolioValue: String) {
+    val portfolio = remember {
+        mutableStateOf(portfolioValue)
+    }
     AddGrayBody1Title(titleText = "포트폴리오 URL") {
         SmsTextField(
             modifier = Modifier.fillMaxWidth(),
-            setText = "",
+            setText = portfolio.value,
             placeHolder = "포트폴리오 URL",
-            onValueChange = {}) {
-
+            onValueChange = { portfolio.value = it }) {
+            portfolio.value = ""
         }
     }
 }
@@ -23,5 +28,5 @@ fun PortfolioComponent() {
 @Preview
 @Composable
 private fun PortfolioComponentPre() {
-    PortfolioComponent()
+    PortfolioComponent("https://youtube.com")
 }
