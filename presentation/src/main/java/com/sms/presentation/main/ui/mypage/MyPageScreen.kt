c841e24a3dc8e2831623ad4ba5edc056e03475b9
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-09 17:20:19 +0900

    ✨ :: Implement the bottom sheet

--- presentation/src/main/java/com/sms/presentation/main/ui/mypage/MyPageScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/MyPageScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.msg.sms.design.component.bottomsheet.SelectorBottomSheet
+import com.msg.sms.design.component.selector.MajorSelector
 import com.sms.presentation.main.ui.mypage.modal.MyPageBottomSheet
 import kotlinx.coroutines.launch
 
@@ -19,28 +23,71 @@ fun MyPageScreen() {
         rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
     val coroutineScope = rememberCoroutineScope()
 
+    val list = remember {
+        mutableStateOf(listOf("Android", "iOS", "Back-End"))
+    }
+    val selectedMajor = remember {
+        mutableStateOf("")
+    }
+    val isMajor = remember {
+        mutableStateOf(false)
+    }
+
     ModalBottomSheetLayout(
         sheetContent = {
-            MyPageBottomSheet(
-                onClickLogout = {
-                    coroutineScope.launch {
-                        bottomSheetState.hide()
+            if (isMajor.value) {
+                SelectorBottomSheet(
+                    // Todo(LeeHyeonbin): 바텀 시트 불러오는 로직 사용해서 리스트 열 때나, 언제든 예외처리 하기
+//                    list = if (list.value.data != null) list.value.data!!.major else emptyList(),
+                    list = list.value,
+                    bottomSheetState = bottomSheetState,
+                    selected = selectedMajor.value,
+                    itemChange = {
+                        selectedMajor.value = it
+                    },
+                    lastItem = {
+                        MajorSelector(
+                            major = "직접입력",
+                            selected = selectedMajor.value == "직접입력"
+                        ) {
+                            selectedMajor.value = "직접입력"
+                            coroutineScope.launch {
+                                bottomSheetState.hide()
+                            }
+                        }
                     }
-                },
-                onClickWithdrawal = {
-                    coroutineScope.launch {
-                        bottomSheetState.hide()
-                    }
-                })
+                )
+            } else {
+                MyPageBottomSheet(
+                    onClickLogout = {
+                        coroutineScope.launch {
+                            bottomSheetState.hide()
+                        }
+                    },
+                    onClickWithdrawal = {
+                        coroutineScope.launch {
+                            bottomSheetState.hide()
+                        }
+                    })
+            }
         },
         sheetState = bottomSheetState,
         sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
     ) {
-        MyPageComponent(clickTopLeftButton = {}, clickTopRightButton = {
-            coroutineScope.launch {
-                bottomSheetState.show()
-            }
-        })
+        MyPageComponent(
+            setMajor = selectedMajor.value,
+            clickTopLeftButton = {},
+            clickTopRightButton = {
+                coroutineScope.launch {
+                    isMajor.value = false
+                    bottomSheetState.show()
+                }
+            }, onClickMajorButton = {
+                coroutineScope.launch {
+                    isMajor.value = true
+                    bottomSheetState.show()
+                }
+            })
     }
 }
 
