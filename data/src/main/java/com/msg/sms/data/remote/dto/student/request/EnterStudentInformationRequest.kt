a2072360e7002277c8ec9d42e23eb8b7114d64c8
khs3994 <z01039948003@gmail.com>
2023-08-18 11:56:31 +0900

    :memo: :: 정보기입 dto에 프로젝트, 수상경력 추가 & SerializedName으로 이름 명시

--- data/src/main/java/com/msg/sms/data/remote/dto/student/request/EnterStudentInformationRequest.kt
+++ data/src/main/java/com/msg/sms/data/remote/dto/student/request/EnterStudentInformationRequest.kt
@@ -1,17 +1,36 @@
 package com.msg.sms.data.remote.dto.student.request
 
+import com.google.gson.annotations.SerializedName
+
 data class EnterStudentInformationRequest(
+    @SerializedName("major")
     val major: String,
+    @SerializedName("techStack")
     val techStack: List<String>,
+    @SerializedName("profileImgUrl")
     val profileImgUrl: String,
+    @SerializedName("introduce")
     val introduce: String,
+    @SerializedName("portfolioUrl")
     val portfolioUrl: String,
+    @SerializedName("contactEmail")
     val contactEmail: String,
+    @SerializedName("formOfEmployment")
     val formOfEmployment: String,
+    @SerializedName("gsmAuthenticationScore")
     val gsmAuthenticationScore: Int,
+    @SerializedName("salary")
     val salary: Int,
+    @SerializedName("region")
     val region: List<String>,
+    @SerializedName("languageCertificate")
     val languageCertificate: List<CertificateData>,
+    @SerializedName("militaryService")
     val militaryService: String,
-    val certificate: List<String>
+    @SerializedName("certificate")
+    val certificate: List<String>,
+    @SerializedName("projects")
+    val projects: List<ProjectData>,
+    @SerializedName("prize")
+    val prize: List<PrizeData>
 )
