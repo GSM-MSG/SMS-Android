52bbe3559529b0ad0cc35745df1efd816f17c555
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-21 22:55:37 +0900

    ✨ :: Create the error color

--- design-system/src/main/java/com/msg/sms/design/component/button/ButtonState.kt
+++ design-system/src/main/java/com/msg/sms/design/component/button/ButtonState.kt
@@ -1,6 +1,8 @@
 package com.msg.sms.design.component.button
 
-sealed class ButtonState() {
+sealed class ButtonState {
     object OutLine: ButtonState()
     object Normal: ButtonState()
+
+    object Error: ButtonState()
 }
