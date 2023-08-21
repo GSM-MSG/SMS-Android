1a8c9746a24e350a060baf8803bb9b3421454acf
khs3994 <z01039948003@gmail.com>
2023-08-21 18:16:42 +0900

    :fire: :: 기존 코드 수정

--- design-system/src/main/java/com/msg/sms/design/component/picker/SmsNumberPicker.kt
+++ design-system/src/main/java/com/msg/sms/design/component/picker/SmsNumberPicker.kt
@@ -1,13 +1,13 @@
 package com.msg.sms.design.component.picker
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,31 +17,17 @@ import com.msg.sms.design.theme.SMSTheme
 @Composable
 fun SmsNumberPicker(itemList: List<String>) {
     val listState = rememberLazyListState()
-    LaunchedEffect("ListState") {
-    }
+
 
     SMSTheme { colors, typography ->
-        Box(modifier = Modifier.wrapContentSize()) {
-            LazyColumn(
-                modifier = Modifier
-                    .width(150.dp)
-                    .height(163.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                state = listState
-            ) {
-                item { Spacer(modifier = Modifier.height(32.dp)) }
-                itemsIndexed(itemList) { index, item ->
-                    Text(
-                        text = item,
-                        modifier = Modifier
-                            .padding(horizontal = 12.dp, vertical = 5.dp),
-                        style = typography.title1,
-                        color = colors.BLACK,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-                item { Spacer(modifier = Modifier.height(32.dp)) }
+        LazyColumn(state = listState) {
+            itemsIndexed(itemList) { idx, item ->
+                Text(
+                    text = item,
+                    style = typography.title1,
+                    color = colors.BLACK,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
@@ -50,26 +36,29 @@ fun SmsNumberPicker(itemList: List<String>) {
 @Preview
 @Composable
 fun SmsNumberPickerPre() {
-    SmsNumberPicker(
-        itemList = listOf(
-            "2012",
-            "2013",
-            "2014",
-            "2015",
-            "2016",
-            "2017",
-            "2018",
-            "2019",
-            "2020",
-            "2021",
-            "2022",
-            "2023",
-            "2024",
-            "2025",
-            "2026",
-            "2027",
-            "2028",
-            "2029"
+    Column {
+        Spacer(modifier = Modifier.height(30.dp))
+        SmsNumberPicker(
+            itemList = listOf(
+                "2012",
+                "2013",
+                "2014",
+                "2015",
+                "2016",
+                "2017",
+                "2018",
+                "2019",
+                "2020",
+                "2021",
+                "2022",
+                "2023",
+                "2024",
+                "2025",
+                "2026",
+                "2027",
+                "2028",
+                "2029"
+            )
         )
-    )
+    }
 }
