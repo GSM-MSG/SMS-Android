2d4af6454826eebeaaa853fdb45ea6c406f0179a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-22 22:59:13 +0900

    ✨ :: Create the ExpandableAwardData data class

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/state/ExpandableAwardDate.kt
@@ -0,0 +1,8 @@
+package com.sms.presentation.main.ui.mypage.state
+
+data class ExpandableAwardDate(
+    val title: String,
+    val organization: String,
+    val date: String,
+    val isExpand: Boolean
+)
