84972aa10aec0f7923daa3c1ef3dfb95a29e4981
khs3994 <z01039948003@gmail.com>
2023-08-22 12:28:36 +0900

    :lipstick: :: 컴포넌트 퍼블리싱

--- design-system/src/main/java/com/msg/sms/design/component/picker/SmsNumberPicker.kt
+++ design-system/src/main/java/com/msg/sms/design/component/picker/SmsNumberPicker.kt
@@ -1,14 +1,20 @@
 package com.msg.sms.design.component.picker
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,16 +24,42 @@ import com.msg.sms.design.theme.SMSTheme
 fun SmsNumberPicker(itemList: List<String>) {
     val listState = rememberLazyListState()
 
-
     SMSTheme { colors, typography ->
-        LazyColumn(state = listState) {
-            itemsIndexed(itemList) { idx, item ->
-                Text(
-                    text = item,
-                    style = typography.title1,
-                    color = colors.BLACK,
-                    fontWeight = FontWeight.Bold
-                )
+        Box {
+            Box(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(32.dp)
+                    .align(Alignment.Center)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(colors.N10)
+                    .onGloballyPositioned {
+
+                    }
+            )
+            LazyColumn(
+                state = listState,
+                modifier = Modifier
+                    .height(163.dp)
+                    .width(150.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Spacer(modifier = Modifier.height(64.dp))
+                }
+                itemsIndexed(itemList) { idx, item ->
+                    Text(
+                        text = item,
+                        style = typography.title1,
+                        color = colors.BLACK,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(vertical = 3.dp)
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(64.dp))
+                }
             }
         }
     }
