bd337938c4197f477783b75815bd8be4418d4f61
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-24 10:37:58 +0900

    👽 :: Change the api name and insert serialized name according to modify api

--- data/src/main/java/com/msg/sms/data/remote/dto/stack/reponse/SearchingDetailStackResponse.kt
+++ data/src/main/java/com/msg/sms/data/remote/dto/stack/reponse/SearchingDetailStackResponse.kt
@@ -1,8 +1,10 @@
 package com.msg.sms.data.remote.dto.stack.reponse
 
+import com.google.gson.annotations.SerializedName
 import com.msg.sms.domain.model.stack.DetailStackListModel
 
 data class SearchingDetailStackResponse(
+    @SerializedName("techStacks")
     val techStack: List<String>
 )
 
