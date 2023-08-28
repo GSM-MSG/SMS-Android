49f7118f800c0978f4031c02ae9e06684798ecf5
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-08-29 08:52:56 +0900

    ✨ :: Create the MilitaryService enum class

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/mypage/state/MilitaryService.kt
@@ -0,0 +1,9 @@
+package com.sms.presentation.main.ui.mypage.state
+
+enum class MilitaryService(val text: String) {
+    HOPE("희망"),
+    NOT_HOPE("비희망"),
+    NO_MATTER("상관없음"),
+    NONE("해당 없음"),
+    NOT_SELECT("")
+}
