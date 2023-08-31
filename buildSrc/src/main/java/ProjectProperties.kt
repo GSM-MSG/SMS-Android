02dcf8175700449a61d6ccb5d619fde3d0f0e411
khs3994 <z01039948003@gmail.com>
2023-09-01 02:22:35 +0900

    :rocket: :: versionname, versioncode

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -20,8 +20,8 @@ object ProjectProperties {
         const val MIN_SDK = 24
         const val TARGET_SDK = 33
         const val JVM_TARGET = "1.8"
-        const val VERSION_CODE = 5
-        const val VERSION_NAME = "1.0.0"
+        const val VERSION_CODE = 6
+        const val VERSION_NAME = "1.1.0"
         val JAVA_VERSION = JavaVersion.VERSION_1_8
     }
 
