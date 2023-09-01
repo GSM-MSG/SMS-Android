2c07877310aafa589c10feee1c7c74615964ca24
khs3994 <z01039948003@gmail.com>
2023-09-01 09:12:17 +0900

    :memo: :: git pull

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -21,7 +21,7 @@ object ProjectProperties {
         const val TARGET_SDK = 33
         const val JVM_TARGET = "1.8"
         const val VERSION_CODE = 6
-        const val VERSION_NAME = "1.1.0"
+        const val VERSION_NAME = "1.1.0-release"
         val JAVA_VERSION = JavaVersion.VERSION_1_8
     }
 
