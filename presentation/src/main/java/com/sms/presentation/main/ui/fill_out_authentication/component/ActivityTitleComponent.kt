96d31698e73a1dbe771891fd3ec826c19311124c
송현서 <s22026@gsm.hs.kr>
2024-02-12 17:53:26 +0900

    :sparkles: :: Add ActivityTitleComponent

---
+++ presentation/src/main/java/com/sms/presentation/main/ui/fill_out_authentication/component/ActivityTitleComponent.kt
@@ -0,0 +1,31 @@
+package com.sms.presentation.main.ui.fill_out_authentication.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.msg.sms.design.component.textfield.SmsTextField
+import com.msg.sms.design.util.AddGrayBody1Title
+
+@Composable
+fun ActivityTitleComponent(
+    activityTitleValue: String,
+    onValueChange: (value: String) -> Unit
+) {
+    AddGrayBody1Title(titleText = "활동 제목") {
+        SmsTextField(
+            modifier = Modifier.fillMaxWidth(),
+            setText = activityTitleValue,
+            placeHolder = "활동 제목 입력",
+            onValueChange = onValueChange
+        ) {
+            onValueChange("")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ActivityTitleComponentPre() {
+    ActivityTitleComponent("Droid Knights 2024", onValueChange = {})
+}
