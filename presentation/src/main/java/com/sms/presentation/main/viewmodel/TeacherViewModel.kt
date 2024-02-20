f04459f1083028b56999a5c0c77069eb2de58793
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2024-02-21 03:31:42 +0900

    :sparkles: 교장선생님 가입 ViewModel 기능 추가

--- presentation/src/main/java/com/sms/presentation/main/viewmodel/TeacherViewModel.kt
+++ presentation/src/main/java/com/sms/presentation/main/viewmodel/TeacherViewModel.kt
@@ -4,6 +4,7 @@ import com.sms.presentation.main.viewmodel.util.Event
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.sms.domain.usecase.teacher.CommonRegistrationUseCase
+import com.msg.sms.domain.usecase.teacher.PrincipalRegistrationUseCase
 import com.sms.presentation.main.viewmodel.util.errorHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,10 +16,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TeacherViewModel @Inject constructor(
-    private val commonRegistrationUseCase: CommonRegistrationUseCase
+    private val commonRegistrationUseCase: CommonRegistrationUseCase,
+    private val principalRegistrationUseCase: PrincipalRegistrationUseCase
 ) : ViewModel() {
     private val _commonResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
     val commonResponse = _commonResponse.asStateFlow()
+
+    private val _principalResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
+    val principalResponse = _principalResponse.asStateFlow()
 
     fun common() = viewModelScope.launch {
         commonRegistrationUseCase().onSuccess {
@@ -29,6 +34,18 @@ class TeacherViewModel @Inject constructor(
             }
         }.onFailure {
             _commonResponse.value = it.errorHandling()
+        }
+    }
+
+    fun principal() = viewModelScope.launch {
+        principalRegistrationUseCase().onSuccess {
+            it.catch {remoteError ->
+                _principalResponse.value = remoteError.errorHandling()
+            }.collect { response ->
+                _principalResponse.value = Event.Success(data = response)
+            }
+        }.onFailure {
+            _principalResponse.value = it.errorHandling()
         }
     }
 }
