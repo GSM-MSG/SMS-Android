81709a307575ee77a558119081683ffe52a243ab
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2024-02-22 01:41:15 +0900

    :sparkles: 담임선생님 가입 Repository 추가

--- domain/src/main/java/com/msg/sms/domain/repository/TeacherRepository.kt
+++ domain/src/main/java/com/msg/sms/domain/repository/TeacherRepository.kt
@@ -1,5 +1,6 @@
 package com.msg.sms.domain.repository
 
+import com.msg.sms.domain.model.teacher.request.HomeroomTeacherRequestModel
 import kotlinx.coroutines.flow.Flow
 
 interface TeacherRepository {
@@ -10,4 +11,6 @@ interface TeacherRepository {
     suspend fun vicePrincipal(): Flow<Unit>
 
     suspend fun headOfDepartment(): Flow<Unit>
+
+    suspend fun homeroom(body: HomeroomTeacherRequestModel): Flow<Unit>
 }
