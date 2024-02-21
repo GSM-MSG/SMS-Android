f9b7a05f06682e563bddf0d5703ed2f92e3eafee
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2024-02-22 01:41:15 +0900

    :sparkles: 담임선생님 가입 Repository 추가

--- data/src/main/java/com/msg/sms/data/repository/TeacherRepositoryImpl.kt
+++ data/src/main/java/com/msg/sms/data/repository/TeacherRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.msg.sms.data.repository
 
 import com.msg.sms.data.remote.datasource.teacher.RemoteTeacherDataSource
+import com.msg.sms.data.remote.dto.teacher.request.HomeroomTeacherRequest
+import com.msg.sms.domain.model.teacher.request.HomeroomTeacherRequestModel
 import com.msg.sms.domain.repository.TeacherRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class TeacherRepositoryImpl @Inject constructor(
@@ -22,5 +25,14 @@ class TeacherRepositoryImpl @Inject constructor(
 
     override suspend fun headOfDepartment(): Flow<Unit> {
         return remoteTeacherDataSource.headOfDepartment()
+    }
+
+    override suspend fun homeroom(body: HomeroomTeacherRequestModel): Flow<Unit> {
+        return remoteTeacherDataSource.homeroom(
+            body = HomeroomTeacherRequest(
+                grade = body.grade,
+                classNum = body.classNum
+            )
+        )
     }
 }
