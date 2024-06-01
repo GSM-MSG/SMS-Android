0db8b9e169708819bacb28b814be0278f611ac06
khs3994 <dev.hyunsss@gmail.com>
2024-06-01 23:54:28 +0900

    :lipstick: :: refactor basic textfield

--- design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
+++ design-system/src/main/java/com/msg/sms/design/component/textfield/SmsTextField.kt
@@ -1,37 +1,35 @@
 package com.msg.sms.design.component.textfield
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msg.sms.design.icon.DeleteButtonIcon
+import com.msg.sms.design.modifier.smsClickable
 import com.msg.sms.design.theme.SMSTheme
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SmsBasicTextField(
     modifier: Modifier = Modifier,
@@ -40,14 +38,12 @@ fun SmsBasicTextField(
     readOnly: Boolean = false,
     isError: Boolean = false,
     errorText: String = "Error",
-    assistiveText: String = "",
     maxLines: Int = Int.MAX_VALUE,
     singleLine: Boolean = false,
     focusManager: FocusManager = LocalFocusManager.current,
     focusRequester: FocusRequester = FocusRequester(),
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
-    label: @Composable (() -> Unit)? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
     leadingIcon: @Composable (() -> Unit)? = null,
     onValueChange: (String) -> Unit = {},
@@ -84,37 +80,41 @@ fun SmsBasicTextField(
                 textStyle = typography.body1,
                 readOnly = readOnly,
                 decorationBox = { innerTextField ->
-                    TextFieldDefaults.OutlinedTextFieldDecorationBox(
-                        value = text,
-                        innerTextField = innerTextField,
-                        enabled = true,
-                        singleLine = singleLine,
-                        visualTransformation = VisualTransformation.None,
-                        interactionSource = remember { MutableInteractionSource() },
-                        placeholder = {
-                            Text(text = placeHolder, style = typography.body1)
-                        },
-                        trailingIcon = trailingIcon,
-                        leadingIcon = leadingIcon,
-                        label = label,
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            backgroundColor = colors.N10,
-                            placeholderColor = colors.N30,
-                            focusedBorderColor = Color.Transparent,
-                            unfocusedBorderColor = Color.Transparent,
-                            cursorColor = colors.P2
-                        ),
-                        contentPadding = PaddingValues(vertical = 13.5.dp, horizontal = 12.dp)
-                    )
+                    Box(
+                        modifier = modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(colors.N10)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(vertical = 13.5.dp, horizontal = 12.dp)
+                        ) {
+                            innerTextField()
+                            if (text.isEmpty()) {
+                                Text(
+                                    text = placeHolder,
+                                    color = colors.N30,
+                                    style = typography.body1,
+                                )
+                            }
+                        }
+                        if (trailingIcon != null) {
+                            Box(modifier = Modifier.align(Alignment.CenterEnd)) {
+                                trailingIcon()
+                            }
+                        }
+                        if (leadingIcon != null) {
+                            Box(modifier = Modifier.align(Alignment.CenterEnd)) {
+                                leadingIcon()
+                            }
+                        }
+                    }
                 }
             )
             if (isError) {
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(text = errorText, color = colors.ERROR, style = typography.caption1)
-            }
-            if (assistiveText.isNotEmpty()) {
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = assistiveText, color = Color(0xFFA0ACB1), style = typography.caption1)
             }
         }
     }
@@ -152,8 +152,15 @@ fun SmsOnlyInputTextField(
         keyboardActions = keyboardActions,
         onValueChange = onValueChange,
         trailingIcon = {
-            IconButton(onClick = onDeleteButtonClick, enabled = text.isNotEmpty()) {
-                if (text.isNotEmpty()) DeleteButtonIcon()
+            if (text.isNotEmpty()) {
+                DeleteButtonIcon(
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .smsClickable(
+                            rippleEnabled = false,
+                            onClick = onDeleteButtonClick
+                        )
+                )
             }
         }
     )
@@ -170,8 +177,7 @@ fun SmsBasicTextFieldPre() {
         text = text,
         onValueChange = onValueChange,
         isError = true,
-        errorText = "error text",
-        assistiveText = "assistive text"
+        errorText = "error text"
     )
 }
 
@@ -185,8 +191,6 @@ fun SmsTextFieldPre() {
     SmsOnlyInputTextField(
         text = text,
         onValueChange = onValueChange,
-        onDeleteButtonClick = {
-
-        }
+        onDeleteButtonClick = {}
     )
 }
