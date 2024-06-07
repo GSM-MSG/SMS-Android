e151c6609c1b009a867bee3cd957112b7b162f57
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2024-06-07 16:14:00 +0900

    kotlin 과 compose 의 버전을 변경합니다.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val GRADLE = "7.4.2"
-    const val KOTLIN = "1.7.0"
+    const val KOTLIN = "1.8.0"
 
-    const val COMPOSE = "1.2.0"
+    const val COMPOSE = "1.4.0"
     const val APPCOMPAT = "1.6.1"
     const val ACTIVITY_COMPOSE = "1.7.2"
     const val COMPOSE_MATERIAL = "1.4.3"
