27006f4daf096f641d8d2832a9552280007a3ce0
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2024-06-12 23:29:27 +0900

    객체를 변경합니다.

--- data/src/main/java/com/msg/sms/data/remote/dto/athentication/AuthenticationFormResponse.kt
+++ data/src/main/java/com/msg/sms/data/remote/dto/athentication/AuthenticationFormResponse.kt
@@ -1,85 +1,90 @@
 package com.msg.sms.data.remote.dto.athentication
 
+import com.msg.sms.domain.model.authentication.AuthenticationAreaFieldModel
+import com.msg.sms.domain.model.authentication.AuthenticationAreaModel
 import com.msg.sms.domain.model.authentication.AuthenticationFormModel
+import com.msg.sms.domain.model.authentication.AuthenticationSectionFieldModel
+import com.msg.sms.domain.model.authentication.AuthenticationSectionFieldValuesModel
+import com.msg.sms.domain.model.authentication.AuthenticationSectionModel
+import com.msg.sms.domain.model.authentication.AuthenticationSelectionType
 
 data class AuthenticationFormResponse(
-    val content: List<AuthenticationArea>,
+    val contents: List<AuthenticationArea>,
+    val files: List<AuthenticationAreaField>,
 ) {
-    data class AuthenticationArea(
-        val title: String,
-        val field: List<AuthenticationAreaField>,
-        val items: List<AuthenticationSection>,
-    ) {
-
-        fun toAuthenticationAreaModel() = AuthenticationFormModel.AuthenticationAreaModel(
-            title = this.title,
-            field = this.field.map { it.toAuthenticationAreaFieldModel() },
-            items = this.items.map { it.toAuthenticationSectionModel() }
-        )
-    }
-
-    data class AuthenticationAreaField(
-        val name: String,
-        val url: String,
-    ) {
-
-        fun toAuthenticationAreaFieldModel() = AuthenticationFormModel.AuthenticationAreaFieldModel(
-            name = this.name,
-            url = this.url
-        )
-    }
-
-    data class AuthenticationSection(
-        val section: String,
-        val scoreDescription: String,
-        val sectionScore: Int,
-        val maxCount: Int,
-        val fields: List<AuthenticationSectionField>,
-    ) {
-        data class AuthenticationSectionField(
-            val key: String,
-            val type: String,
-            val values: List<AuthenticationSectionFieldValues>?,
-            val example: String,
-        ) {
-            data class AuthenticationSectionFieldValues(
-                val selectId: String,
-                val value: String,
-            ) {
-
-                fun toAuthenticationSectionFieldValuesModel() =
-                    AuthenticationFormModel.AuthenticationSectionModel.AuthenticationSectionFieldModel.AuthenticationSectionFieldValues(
-                        selectId = this.selectId,
-                        value = this.value
-                    )
-            }
-
-            fun toAuthenticationSectionFieldModel() =
-                AuthenticationFormModel.AuthenticationSectionModel.AuthenticationSectionFieldModel(
-                    key = this.key,
-                    type = AuthenticationFormModel.AuthenticationSectionModel.AuthenticationSelectionType.valueOf(
-                        this.type
-                    ),
-                    values = this.values?.map { it.toAuthenticationSectionFieldValuesModel() },
-                    example = this.example
-                )
-        }
-
-        fun toAuthenticationSectionModel() = AuthenticationFormModel.AuthenticationSectionModel(
-            section = this.section,
-            scoreDescription = this.scoreDescription,
-            sectionScore = this.sectionScore,
-            maxCount = this.maxCount,
-            fields = this.fields.map { it.toAuthenticationSectionFieldModel() },
-        )
-    }
-
     fun toAuthenticationFormModel(): AuthenticationFormModel {
         return AuthenticationFormModel(
-            content = content.map { it.toAuthenticationAreaModel() }
+            contents = contents.map { it.toAuthenticationAreaModel() },
+            files = files.map { it.toAuthenticationAreaFieldModel() }
         )
     }
 }
 
+data class AuthenticationArea(
+    val title: String,
+    val sections: List<AuthenticationSection>,
+) {
+    fun toAuthenticationAreaModel() = AuthenticationAreaModel(
+        title = this.title,
+        sections = this.sections.map { it.toAuthenticationSectionModel() }
+    )
+}
 
+data class AuthenticationAreaField(
+    val name: String,
+    val url: String,
+) {
 
+    fun toAuthenticationAreaFieldModel() = AuthenticationAreaFieldModel(
+        name = this.name,
+        url = this.url
+    )
+}
+
+data class AuthenticationSection(
+    val section: String,
+    val sectionId: String,
+    val scoreDescription: String,
+    val sectionScore: Int,
+    val maxCount: Int,
+    val fields: List<AuthenticationSectionField>,
+) {
+
+    fun toAuthenticationSectionModel() = AuthenticationSectionModel(
+        section = this.section,
+        scoreDescription = this.scoreDescription,
+        sectionScore = this.sectionScore,
+        maxCount = this.maxCount,
+        fields = this.fields.map { it.toAuthenticationSectionFieldModel() },
+        sectionId = this.sectionId
+    )
+
+}
+
+data class AuthenticationSectionFieldValues(
+    val selectId: String,
+    val value: String,
+) {
+    fun toAuthenticationSectionFieldValuesModel() =
+        AuthenticationSectionFieldValuesModel(
+            selectId = this.selectId,
+            value = this.value
+        )
+}
+
+data class AuthenticationSectionField(
+    val key: String,
+    val type: String,
+    val values: List<AuthenticationSectionFieldValues>?,
+    val example: String,
+) {
+
+    fun toAuthenticationSectionFieldModel() =
+        AuthenticationSectionFieldModel(
+            type = AuthenticationSelectionType.valueOf(
+                this.type
+            ),
+            values = this.values?.map { it.toAuthenticationSectionFieldValuesModel() },
+            example = this.example
+        )
+}
--- domain/src/main/java/com/msg/sms/domain/model/authentication/AuthenticationFormModel.kt
+++ domain/src/main/java/com/msg/sms/domain/model/authentication/AuthenticationFormModel.kt
@@ -1,41 +1,36 @@
 package com.msg.sms.domain.model.authentication
 
 data class AuthenticationFormModel(
-    val content: List<AuthenticationAreaModel>,
-) {
-    data class AuthenticationAreaModel(
-        val title: String,
-        val field: List<AuthenticationAreaFieldModel>,
-        val items: List<AuthenticationSectionModel>,
-    )
+    val contents: List<AuthenticationAreaModel>,
+    val files: List<AuthenticationAreaFieldModel>,
+)
 
-    data class AuthenticationAreaFieldModel(
-        val name: String,
-        val url: String,
-    )
+data class AuthenticationAreaModel(
+    val title: String,
+    val sections: List<AuthenticationSectionModel>,
+)
 
-    data class AuthenticationSectionModel(
-        val section: String,
-        val scoreDescription: String?,
-        val sectionScore: Int,
-        val maxCount: Int,
-        val fields: List<AuthenticationSectionFieldModel>,
-    ) {
-        data class AuthenticationSectionFieldModel(
-            val key: String,
-            val type: AuthenticationSelectionType,
-            val values: List<AuthenticationSectionFieldValues>?,
-            val example: String?,
-        ) {
-            data class AuthenticationSectionFieldValues(
-                val selectId: String,
-                val value: String,
-            )
-        }
+data class AuthenticationAreaFieldModel(
+    val name: String,
+    val url: String,
+)
 
-        enum class AuthenticationSelectionType {
-            TEXT, FILE, BOOLEAN, SELECT_VALUE
-        }
-    }
-}
+data class AuthenticationSectionModel(
+    val sectionId: String,
+    val section: String,
+    val scoreDescription: String?,
+    val sectionScore: Int,
+    val maxCount: Int,
+    val fields: List<AuthenticationSectionFieldModel>,
+)
 
+data class AuthenticationSectionFieldModel(
+    val type: AuthenticationSelectionType,
+    val values: List<AuthenticationSectionFieldValuesModel>?,
+    val example: String?,
+)
+
+data class AuthenticationSectionFieldValuesModel(
+    val selectId: String,
+    val value: String,
+)
