a1f31cb7304538a2bd3e2b439a6b1f5948d9fa02
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2024-06-12 23:27:30 +0900

    폼 조회하는 api 구현

--- data/src/main/java/com/msg/sms/data/remote/network/api/AuthenticationAPI.kt
+++ data/src/main/java/com/msg/sms/data/remote/network/api/AuthenticationAPI.kt
@@ -2,9 +2,10 @@ package com.msg.sms.data.remote.network.api
 
 import com.msg.sms.data.remote.dto.athentication.AuthenticationFormResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AuthenticationAPI {
 
-    @GET("authentication/")
-    suspend fun fetchAuthenticationForm(): AuthenticationFormResponse
+    @GET("authentication/form/{uuid}")
+    suspend fun fetchAuthenticationForm(@Path("uuid") uuid: String): AuthenticationFormResponse
 }
