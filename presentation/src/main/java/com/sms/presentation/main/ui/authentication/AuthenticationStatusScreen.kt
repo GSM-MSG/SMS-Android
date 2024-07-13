65d1b2d929d7b77c59cfc5d4916c4a1e5faf5b62
김현승 <80810303+khs3994@users.noreply.github.com>
2024-07-13 16:21:46 +0900

    :sparkles: :: 텍스트 얼라인 센터 추가 (#479)

--- presentation/src/main/java/com/sms/presentation/main/ui/authentication/AuthenticationStatusScreen.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/authentication/AuthenticationStatusScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msg.sms.design.theme.SMSTheme
@@ -52,6 +53,7 @@ fun AuthenticationStatusComponent(
                             style = typography.title2,
                             fontWeight = FontWeight.Bold,
                             color = colors.N40,
+                            textAlign = TextAlign.Center
                         )
                     }
 
