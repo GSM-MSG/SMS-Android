2856621424da3c65fbd8b37dcedaa8fd2d4e8c07
김현승 <80810303+khs3994@users.noreply.github.com>
2024-07-14 21:40:43 +0900

    :sparkles: :: 인증제 페이지 진입시 보여줄 로티를 추가하였습니다. (#485)

--- presentation/src/main/java/com/sms/presentation/main/ui/authentication/AuthenticationRoute.kt
+++ presentation/src/main/java/com/sms/presentation/main/ui/authentication/AuthenticationRoute.kt
@@ -2,12 +2,19 @@ package com.sms.presentation.main.ui.authentication
 
 import android.webkit.MimeTypeMap
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.msg.sms.design.component.lottie.SmsLoadingLottie
 import com.msg.sms.design.component.topbar.TopNavigation
 import com.msg.sms.design.icon.BackButtonIcon
 import com.msg.sms.domain.model.authentication.MarkingBoardType
@@ -46,39 +53,58 @@ fun AuthenticationRoute(
             leftIcon = { BackButtonIcon() },
             onClickLeftButton = onBackPressed
         )
-        if (verifyAuthenticationState.value is Event.Success && verifyAuthenticationData.value?.markingBoardType == MarkingBoardType.NOT_SUBMITTED && authenticationForm.value != null) {
-            AuthenticationScreen(
-                authenticationForm = authenticationForm.value!!,
-                downloadFile = {
-                    context.downloadFile(url = it.url, fileName = it.name)
-                },
-                submitAuthenticationForm = {
-                    viewModel.submitAuthenticationForm(
-                        context = context,
-                        userData = it.values.toList()
-                    )
-                },
-                showExtensionError = {
-                    Toast.makeText(
-                        context,
-                        "지원하지 않는 파일 형식입니다. hwp, hwpx, pdf 형식만 업로드 가능합니다.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                },
-                getFileName = {
-                    val contentResolve = context.contentResolver
-                    var fileName: String? = null
+        when {
+            verifyAuthenticationState.value is Event.Success && verifyAuthenticationData.value?.markingBoardType == MarkingBoardType.NOT_SUBMITTED && authenticationForm.value != null -> {
+                AuthenticationScreen(
+                    authenticationForm = authenticationForm.value!!,
+                    downloadFile = {
+                        context.downloadFile(url = it.url, fileName = it.name)
+                    },
+                    submitAuthenticationForm = {
+                        viewModel.submitAuthenticationForm(
+                            context = context,
+                            userData = it.values.toList()
+                        )
+                    },
+                    showExtensionError = {
+                        Toast.makeText(
+                            context,
+                            "지원하지 않는 파일 형식입니다. hwp, hwpx, pdf 형식만 업로드 가능합니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    },
+                    getFileName = {
+                        val contentResolve = context.contentResolver
+                        var fileName: String? = null
 
-                    contentResolve.query(it, null, null, null, null)?.use { cursor ->
-                        val nameIndex = cursor.getColumnIndex("_display_name")
-                        cursor.moveToFirst()
-                        fileName = cursor.getString(nameIndex)
+                        contentResolve.query(it, null, null, null, null)?.use { cursor ->
+                            val nameIndex = cursor.getColumnIndex("_display_name")
+                            cursor.moveToFirst()
+                            fileName = cursor.getString(nameIndex)
+                        }
+                        val extension = MimeTypeMap.getSingleton()
+                            .getExtensionFromMimeType(contentResolve.getType(it)) ?: ""
+                        Pair(
+                            fileName ?: (UUID.randomUUID().toString() + "." + extension),
+                            extension
+                        )
                     }
-                    val extension = MimeTypeMap.getSingleton()
-                        .getExtensionFromMimeType(contentResolve.getType(it)) ?: ""
-                    Pair(fileName ?: (UUID.randomUUID().toString() + "." + extension), extension)
+                )
+            }
+
+            verifyAuthenticationState.value is Event.Success && !verifyAuthenticationData.value?.name.isNullOrBlank() && verifyAuthenticationData.value?.markingBoardType != MarkingBoardType.NOT_SUBMITTED -> {
+                AuthenticationStatusComponent(verifyAuthenticationModel = verifyAuthenticationData.value)
+            }
+
+            else -> {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    SmsLoadingLottie(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .align(Alignment.Center),
+                    )
                 }
-            )
-        } else AuthenticationStatusComponent(verifyAuthenticationModel = verifyAuthenticationData.value)
+            }
+        }
     }
 }
