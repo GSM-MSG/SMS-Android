d279f3f1266caac16ce2e5c2230e13d7e078a37f
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2024-08-20 00:17:25 +0900

    version up (1.3.0, 16)

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -20,8 +20,8 @@ object ProjectProperties {
         const val MIN_SDK = 26
         const val TARGET_SDK = 34
         const val JVM_TARGET = "1.8"
-        const val VERSION_CODE = 14
-        const val VERSION_NAME = "1.2.1"
+        const val VERSION_CODE = 16
+        const val VERSION_NAME = "1.3.0"
         val JAVA_VERSION = JavaVersion.VERSION_1_9
     }
 
